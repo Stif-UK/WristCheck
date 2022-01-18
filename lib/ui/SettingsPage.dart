f42bfb1ed62447720366cccbabeca58b45427da4
Grant Hutchison <stif@grants-mbp.lan>
2022-01-18 00:38:41 +0000

    Dark Mode toggle switch base implementation

--- lib/ui/SettingsPage.dart
+++ lib/ui/SettingsPage.dart
@@ -2,17 +2,37 @@ import 'package:flutter/material.dart';
 
 class SettingsPage extends StatefulWidget{
 
-  @override
+@override
   State<SettingsPage> createState() => _SettingsPageState();
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+
+  bool _darkModeToggle = false;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Settings"),
         leading:  IconButton(onPressed: (){Navigator.pop(context);}, icon: const Icon(Icons.arrow_back)),
+      ),
+      body: ListView(
+        children: [
+          SwitchListTile(
+            title: const Text("Dark Mode"),
+            secondary: const Icon(Icons.dark_mode_rounded),
+            value: _darkModeToggle,
+            onChanged: (bool value){
+              setState(() {
+                _darkModeToggle = value;
+                print(_darkModeToggle);
+              });
+
+            },
+          )
+        ],
       ),
 
 
