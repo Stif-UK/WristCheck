1e9e89209736eb4c8ca02e5485796218c462002d
Grant Hutchison <stif@grants-mbp.lan>
2022-08-01 01:06:00 +0100

    basic input form created

--- lib/ui/addWatch.dart
+++ lib/ui/addWatch.dart
@@ -10,9 +10,9 @@ class AddWatch extends StatefulWidget {
 class _AddWatchState extends State<AddWatch> {
 
   //setup input parameters and create form key
-  String _manufacturer = "";
-  String _model = "";
-  String _serialNumber ="";
+  String? _manufacturer;
+  String? _model;
+  String? _serialNumber;
   bool favourite = false;
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
@@ -22,25 +22,41 @@ class _AddWatchState extends State<AddWatch> {
   Widget _buildManufacturerField(){
     return TextFormField(
       decoration: InputDecoration(labelText: "Manufacturer"),
-    //   validator: (String value){
-    //     if(value.isEmpty){
-    //       return "Manufacturer is required";
-    //     }
-    // },
-    // onSaved: (String value){
-    //     _manufacturer = value;
-    // },
+      validator: (value){
+        if(value == null || value.isEmpty){
+          return "Manufacturer is required";
+        }
+    },
+    onSaved: (value){
+        _manufacturer = value;
+    },
     );
   }
 
-  // Widget _buildModelField(){
-  //   return null;
-  // };
-  //
-  // Widget _buildSerialNumberField(){
-  //   return null;
-  // };
-  //
+  Widget _buildModelField(){
+    return TextFormField(
+      decoration: InputDecoration(labelText: "Model"),
+      validator: (value){
+        if(value == null || value.isEmpty){
+          return "Model is required";
+        }
+      },
+      onSaved: (value){
+        _model = value;
+      },
+    );
+  }
+
+  Widget _buildSerialNumberField(){
+    return TextFormField(
+      decoration: InputDecoration(labelText: "Serial Number"),
+      // No validation as null is acceptable
+      onSaved: (value){
+        _serialNumber = value;
+      },
+    );
+  }
+
   // //ToDo:This is a bool field? I think I can remove and replace with a toggle?
   // Widget _buildFavouriteField(){
   //   return null;
@@ -50,6 +66,7 @@ class _AddWatchState extends State<AddWatch> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
         title: Text("Add a watch")
       ),
@@ -61,9 +78,30 @@ class _AddWatchState extends State<AddWatch> {
             child: Column(
             children: [
               _buildManufacturerField(),
-              // _buildModelField(),
-              // _buildSerialNumberField(),
+              _buildModelField(),
+              _buildSerialNumberField(),
               // _buildFavouriteField(),
+              // Switch(value: _favourite, onChanged: (value){
+              //   _favourite = true;
+              // }),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("Favourite:"),
+
+                  Switch(
+                      value: favourite,
+                      onChanged: (value){
+                        setState(
+                                (){
+                              favourite = value;
+                            }
+                        );
+                      }),
+                ]
+
+              ),
+
               SizedBox(height: 100,),
               RaisedButton(
                   child: Text("Add Watch"),
