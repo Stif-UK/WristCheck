5914d2b4b9281d40eb9cf6c8c431c7d8fc9f2a86
Grant Hutchison <stif@grants-mbp.lan>
2022-08-02 22:28:28 +0100

    fixed issue where collection view wouldn't refresh on first watch added

--- lib/ui/WatchBoxWidget.dart
+++ lib/ui/WatchBoxWidget.dart
@@ -22,20 +22,16 @@ class _WatchBoxWidgetState extends State<WatchBoxWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-      //check if watchbox is empty, if true display the 'empty screen'
-      //ToDo: if box goes from empty to 1 entry, the view doesn't auto refresh
-      watchBox.isEmpty?
-      Container(
+      body: ValueListenableBuilder<Box<Watches>>(valueListenable: watchBox.listenable(),
+          builder: (context, box, _){
+
+        return watchBox.isEmpty?Container(
           alignment: Alignment.center,
           child: Text("Your watch-box is currently empty\n\nPress the red button to add watches to your collection\n",
-          textAlign: TextAlign.center,),
+            textAlign: TextAlign.center,),
         ):
-          //if watchbox is not empty populate the screen
-      
-      ValueListenableBuilder<Box<Watches>>(valueListenable: watchBox.listenable(),
-          builder: (context, box, _){
-        return ListView.separated(
+
+         ListView.separated(
             itemCount: watchBox.length,
             itemBuilder: (BuildContext context, int index){
               var watch = watchBox.getAt(index);
