195e71fc2b2f489e44f52f73ca9552e67c93a8ff
Grant Hutchison <stif@grants-mbp.lan>
2022-08-03 19:46:45 +0100

    tiles added to header widget

--- lib/ui/watchbox_header.dart
+++ lib/ui/watchbox_header.dart
@@ -12,7 +12,24 @@ class _WatchBoxHeaderState extends State<WatchBoxHeader> {
   Widget build(BuildContext context) {
     return ExpansionTile(
       title: Text("Filter"),
-      leading: Icon(Icons.filter_alt)
+      leading: Icon(Icons.filter_alt),
+      children:[
+        ListTile(
+          title: Text("Clear filters"),
+          trailing: Icon(Icons.filter_alt_off),
+          onTap: (){},
+        ),
+        ListTile(
+          title: Text("Show Wishlist"),
+          trailing: Icon(Icons.cake_outlined),
+          onTap:(){}
+        ),
+        ListTile(
+            title: Text("Show Sold"),
+            trailing: Icon(Icons.attach_money),
+            onTap:(){}
+        )
+      ]
     );
   }
 }
