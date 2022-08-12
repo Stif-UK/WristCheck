4e0adcff9273b30903d539af65566d61179a8e41
Grant Hutchison <stif@grants-mbp.lan>
2022-08-13 00:56:14 +0100

    added header to servicing widget

--- lib/ui/ServicingWidget.dart
+++ lib/ui/ServicingWidget.dart
@@ -5,6 +5,9 @@ import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:wristcheck/util/list_tile_helper.dart';
+import 'package:get/get.dart';
+import 'package:wristcheck/ui/view_watch.dart';
+
 
 
 class ServicingWidget extends StatefulWidget {
@@ -35,42 +38,52 @@ class _ServicingWidgetState extends State<ServicingWidget> {
                   textAlign: TextAlign.center,),
               ):
 
-              ListView.separated(
-                itemCount: filteredList.length,
-                itemBuilder: (BuildContext context, int index){
-                  var watch = filteredList.elementAt(index);
-                  String? _title = "${watch.manufacturer} ${watch.model}";
+              Column(
+                  children:[
+                    Expanded(
+              flex:1,
+                  child: ListTile(
+                    title: Text("Service Schedule"),
+                    trailing: Icon(Icons.help),
+                    leading: Icon(Icons.schedule),
+                  )
+
+              ),
+                    Divider(
+                      thickness: 2.0,
+                    ),
 
 
-                  return ListTile(
-                    leading: ListTileHelper.getServicingIcon(watch.nextServiceDue!),
-                      // Icon(Icons.manage_history_rounded),
-                    title: Text("$_title"),
-                    subtitle: Text("Next Service: ${DateFormat.yMMMd().format(watch.nextServiceDue!)}"),
-                    // trailing:  fav? Icon(Icons.star): Icon(Icons.star_border),
-                    // onTap: () => Get.to(ViewWatch(currentWatch: watch,)),
-                  );
-                },
-                separatorBuilder: (context, index){
-                  return Divider();
-                },
+                    Expanded(
+              flex: 9,
+              child:ListView.separated(
+              itemCount: filteredList.length,
+              itemBuilder: (BuildContext context, int index){
+              var watch = filteredList.elementAt(index);
+              String? _title = "${watch.manufacturer} ${watch.model}";
+
+
+              return ListTile(
+              leading: ListTileHelper.getServicingIcon(watch.nextServiceDue!),
+              title: Text("$_title"),
+              subtitle: Text("Next Service: ${DateFormat.yMMMd().format(watch.nextServiceDue!)}"),
+              //ToDo: Update and create service view page?
+              onTap: () => Get.to(ViewWatch(currentWatch: watch,)),
               );
+              },
+              separatorBuilder: (context, index){
+              return Divider();
+              },
+              )
+              )
+                  ]
+                  );
             }
 
 
         )
 
     );
-
-
-      // Container(
-      // alignment: Alignment.center,
-      // child:
-      //     Text("No Service schedules identified. \n\nEdit your watch info to track service timelines and last-serviced dates.",
-      //     textAlign: TextAlign.center,),
-      // );
-
-
 
   }
 }
