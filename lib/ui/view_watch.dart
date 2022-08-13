65eb5ad81564274fa104c090e844ac7fb7789938
Grant Hutchison <stif@grants-mbp.lan>
2022-08-13 23:44:48 +0100

    updated const modifiers

--- lib/ui/view_watch.dart
+++ lib/ui/view_watch.dart
@@ -14,33 +14,39 @@ class ViewWatch extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("${currentWatch.manufacturer} ${currentWatch.model}"),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Icon(Icons.edit),
+          )
+        ],
       ),
       body: Container(
-        padding: EdgeInsets.all(20.0),
-        margin: EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(20.0),
+        margin: const EdgeInsets.all(20.0),
         child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text("Manufacturer: ${currentWatch.manufacturer}"),
-          SizedBox(height: 10),
+          const SizedBox(height: 10),
           Text("Model: ${currentWatch.model}"),
-          SizedBox(height: 10),
+          const SizedBox(height: 10),
           Text("Favourite: ${currentWatch.favourite}"),
-          SizedBox(height: 10),
-          currentWatch.serialNumber != null? Text("Serial Number: ${currentWatch.serialNumber}") : Text("Serial Number: Not provided"),
-          SizedBox(height: 10),
+          const SizedBox(height: 10),
+          currentWatch.serialNumber != null? Text("Serial Number: ${currentWatch.serialNumber}") : const Text("Serial Number: Not provided"),
+          const SizedBox(height: 10),
           Text("Status: ${currentWatch.status}"),
-          SizedBox(height: 10),
-          currentWatch.purchaseDate != null? Text("Purchased: ${DateFormat.yMMMd().format(currentWatch.purchaseDate!)}"): Text("Purchase Date: Not Recorded"),
-          SizedBox(height: 10),
-          currentWatch.lastServicedDate != null? Text("Last Serviced: ${DateFormat.yMMMd().format(currentWatch.lastServicedDate!)}"): Text("Last serviced: N/A"),
-          SizedBox(height: 10),
-          currentWatch.serviceInterval != 0? Text("Service every ${currentWatch.serviceInterval} years") : Text("Service interval not recorded"),
-          SizedBox(height: 10),
-          currentWatch.nextServiceDue != null? Text("Next service date: ${DateFormat.yMMMd().format(currentWatch.nextServiceDue!)}"): Text("Next Service date: N/A"),
-          SizedBox(height: 10),
-          currentWatch.notes != null? Text("Notes: \n${currentWatch.notes}") : Text("Notes:")
+          const SizedBox(height: 10),
+          currentWatch.purchaseDate != null? Text("Purchased: ${DateFormat.yMMMd().format(currentWatch.purchaseDate!)}"): const Text("Purchase Date: Not Recorded"),
+          const SizedBox(height: 10),
+          currentWatch.lastServicedDate != null? Text("Last Serviced: ${DateFormat.yMMMd().format(currentWatch.lastServicedDate!)}"): const Text("Last serviced: N/A"),
+          const SizedBox(height: 10),
+          currentWatch.serviceInterval != 0? Text("Service every ${currentWatch.serviceInterval} years") : const Text("Service interval not recorded"),
+          const SizedBox(height: 10),
+          currentWatch.nextServiceDue != null? Text("Next service date: ${DateFormat.yMMMd().format(currentWatch.nextServiceDue!)}"): const Text("Next Service date: N/A"),
+          const SizedBox(height: 10),
+          currentWatch.notes != null? Text("Notes: \n${currentWatch.notes}") : const Text("Notes:")
 
 
 
