2496a423c7691bbfeaeca02fbbd60c7aaa9cb8db
Grant Hutchison <stif@grants-mbp.lan>
2022-08-18 12:36:39 +0100

    tidy up

--- lib/ui/charts/wear_chart.dart
+++ lib/ui/charts/wear_chart.dart
@@ -12,29 +12,36 @@ class WearChart extends StatelessWidget {
   final bool animate;
 
 
-  const WearChart({required this.data, required this.animate});
+
+  WearChart({required this.data, required this.animate});
 
   @override
   Widget build(BuildContext context) {
-    List<charts.Series<Watches, String>> series =[
-      charts.Series(
-        id: "Watches",
-        data: data,
-        domainFn: (Watches series, _) => series.model,
-        measureFn: (Watches series, _) => series.wearList.length,
-          // Set a label accessor to control the text of the bar label.
-          labelAccessorFn: (Watches series, _) =>
-    // '${series.model}: \$${sales.sales.toString()}',
-        "${series.manufacturer} ${series.model}: ${series.wearList.length}"
-      )
-    ];
+
+
+      List<charts.Series<Watches, String>> series =[
+        charts.Series(
+            id: "Watches",
+            data: data,
+            domainFn: (Watches series, _) => series.model,
+            measureFn: (Watches series, _) => series.wearList.length,
+            // Set a label accessor to control the text of the bar label.
+            labelAccessorFn: (Watches series, _) =>
+            // '${series.model}: \$${sales.sales.toString()}',
+            "${series.manufacturer} ${series.model}: ${series.wearList.length}"
+        )
+      ];
+
+
+
 
     return charts.BarChart(series,
     vertical: false,
       // Set a bar label decorator.
       // Example configuring different styles for inside/outside:
             barRendererDecorator: charts.BarLabelDecorator(
-               insideLabelStyleSpec: Get.isDarkMode? const charts.TextStyleSpec(fontSize: 12, color: charts.Color.white):const charts.TextStyleSpec(fontSize: 12, color: charts.Color.black)) ,
+              outsideLabelStyleSpec: Get.isDarkMode? const charts.TextStyleSpec(fontSize: 12, color: charts.Color.white):const charts.TextStyleSpec(fontSize: 12, color: charts.Color.black)) ,
+               // insideLabelStyleSpec: Get.isDarkMode? const charts.TextStyleSpec(fontSize: 12, color: charts.Color.white):const charts.TextStyleSpec(fontSize: 12, color: charts.Color.black)) ,
       //          outsideLabelStyleSpec: new charts.TextStyleSpec(...)),
       // barRendererDecorator: charts.BarLabelDecorator<String>(),
       // Hide domain axis.
@@ -43,5 +50,8 @@ class WearChart extends StatelessWidget {
     );
 
 
+
   }
+
 }
+
