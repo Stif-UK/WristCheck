26338f841259e472b1b8b57825a84b7dbac93edc
Grant Hutchison <stif@Grants-MacBook-Pro.local>
2022-08-18 16:24:43 +0100

    updated bar chart to use syncfusion library

--- lib/ui/charts/wear_chart.dart
+++ lib/ui/charts/wear_chart.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
+// import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:wristcheck/model/watches.dart';
 import 'package:get/get.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 
 class WearChart extends StatelessWidget {
@@ -19,35 +20,52 @@ class WearChart extends StatelessWidget {
   Widget build(BuildContext context) {
 
 
-      List<charts.Series<Watches, String>> series =[
-        charts.Series(
-            id: "Watches",
-            data: data,
-            domainFn: (Watches series, _) => series.model,
-            measureFn: (Watches series, _) => series.wearList.length,
-            // Set a label accessor to control the text of the bar label.
-            labelAccessorFn: (Watches series, _) =>
-            // '${series.model}: \$${sales.sales.toString()}',
-            "${series.manufacturer} ${series.model}: ${series.wearList.length}"
-        )
-      ];
+      // List<charts.Series<Watches, String>> series =[
+      //   charts.Series(
+      //       id: "Watches",
+      //       data: data,
+      //       domainFn: (Watches series, _) => series.model,
+      //       measureFn: (Watches series, _) => series.wearList.length,
+      //       // Set a label accessor to control the text of the bar label.
+      //       labelAccessorFn: (Watches series, _) =>
+      //       // '${series.model}: \$${sales.sales.toString()}',
+      //       "${series.manufacturer} ${series.model}: ${series.wearList.length}"
+      //   )
+      // ];
 
 
 
 
-    return charts.BarChart(series,
-    vertical: false,
-      // Set a bar label decorator.
-      // Example configuring different styles for inside/outside:
-            barRendererDecorator: charts.BarLabelDecorator(
-              outsideLabelStyleSpec: Get.isDarkMode? const charts.TextStyleSpec(fontSize: 12, color: charts.Color.white):const charts.TextStyleSpec(fontSize: 12, color: charts.Color.black)) ,
-               // insideLabelStyleSpec: Get.isDarkMode? const charts.TextStyleSpec(fontSize: 12, color: charts.Color.white):const charts.TextStyleSpec(fontSize: 12, color: charts.Color.black)) ,
-      //          outsideLabelStyleSpec: new charts.TextStyleSpec(...)),
-      // barRendererDecorator: charts.BarLabelDecorator<String>(),
-      // Hide domain axis.
-      domainAxis:
-      const charts.OrdinalAxisSpec(renderSpec: charts.NoneRenderSpec()),
+    return SfCartesianChart(
+      primaryXAxis: CategoryAxis(
+        isVisible: false,
+      ),
+      primaryYAxis: NumericAxis(),
+      series: <BarSeries<Watches, String>>[
+        BarSeries(
+            dataSource: data,
+          xValueMapper: (Watches series, _) => series.model,
+          yValueMapper: (Watches series, _) => series.wearList.length,
+          dataLabelMapper: (watch, _) => "${watch.model}: ${watch.wearList.length}",
+          dataLabelSettings: const DataLabelSettings(isVisible: true)
+      )
+
+      ],
     );
+
+    //   charts.BarChart(series,
+    // vertical: false,
+    //   // Set a bar label decorator.
+    //   // Example configuring different styles for inside/outside:
+    //         barRendererDecorator: charts.BarLabelDecorator(
+    //           outsideLabelStyleSpec: Get.isDarkMode? const charts.TextStyleSpec(fontSize: 12, color: charts.Color.white):const charts.TextStyleSpec(fontSize: 12, color: charts.Color.black)) ,
+    //            // insideLabelStyleSpec: Get.isDarkMode? const charts.TextStyleSpec(fontSize: 12, color: charts.Color.white):const charts.TextStyleSpec(fontSize: 12, color: charts.Color.black)) ,
+    //   //          outsideLabelStyleSpec: new charts.TextStyleSpec(...)),
+    //   // barRendererDecorator: charts.BarLabelDecorator<String>(),
+    //   // Hide domain axis.
+    //   domainAxis:
+    //   const charts.OrdinalAxisSpec(renderSpec: charts.NoneRenderSpec()),
+    // );
 
 
 
