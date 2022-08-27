c869baafe58b9cc33ec5f60ec8024827485534e1
Grant Hutchison <stif@grants-mbp.lan>
2022-08-27 09:56:54 +0100

    Added version number check on status page - may not work on iOS

--- lib/ui/SettingsPage.dart
+++ lib/ui/SettingsPage.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:wristcheck/boxes.dart';
 import 'package:wristcheck/ui/archived.dart';
 import 'package:wristcheck/copy/snackbars.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 
 
 
 class SettingsPage extends StatefulWidget{
+
+
+
 
 @override
   State<SettingsPage> createState() => _SettingsPageState();
@@ -17,7 +22,18 @@ class _SettingsPageState extends State<SettingsPage> {
 
   bool _darkModeToggle = false;
   final watchBox = Boxes.getWatches();
+  String _buildVersion = "Not Determined";
 
+
+  @override
+  void initState() {
+    super.initState();
+    _getBuildVersion().then((val) {
+      setState(() {
+        _buildVersion = val;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,60 +42,78 @@ class _SettingsPageState extends State<SettingsPage> {
         title: const Text("Settings"),
         leading:  IconButton(onPressed: (){Navigator.pop(context);}, icon: const Icon(Icons.arrow_back)),
       ),
-      body: ListView(
+      body: Column(
         children: [
-          SwitchListTile(
-            title: const Text("Dark Mode"),
-            secondary: const Icon(Icons.dark_mode_rounded),
-            value: _darkModeToggle,
-            onChanged: (bool value){
-              Get.changeTheme(
-                  Get.isDarkMode ? ThemeData.light() : ThemeData.dark()
-              );
-              setState(() {
-                _darkModeToggle = value;
+          Expanded(
+            child: ListView(
+              children: [
+                SwitchListTile(
+                  title: const Text("Dark Mode"),
+                  secondary: const Icon(Icons.dark_mode_rounded),
+                  value: _darkModeToggle,
+                  onChanged: (bool value){
+                    Get.changeTheme(
+                        Get.isDarkMode ? ThemeData.light() : ThemeData.dark()
+                    );
+                    setState(() {
+                      _darkModeToggle = value;
 
-                print(_darkModeToggle);
-              });
+                      print(_darkModeToggle);
+                    });
 
-            },
-          ),
-          const Divider(thickness: 2,),
-           ListTile(
-            title: const Text("Show Archived Watches"),
-              leading: const Icon(Icons.archive_outlined),
-            onTap: (){
-              Get.to(()=> Archived());
-            }
-          ),
-          const Divider(thickness: 2,),
-          ListTile(
-            title:const Text("Delete collection"),
-            leading: const Icon(Icons.warning),
-            trailing: OutlinedButton(
-              child: const Icon(Icons.delete, color: Colors.redAccent),
-              onPressed: (){
-                Get.defaultDialog(
-                  title: "Warning",
-                  middleText: "Pressing OK will delete all watch data, including your wishlist\n \n THIS CANNOT BE UNDONE",
-                  textConfirm: "OK",
-                  textCancel: "Cancel",
-                  onConfirm: (){
-                    //On pressing ok, call the .clear() method on the watchbox
-                    watchBox.clear();
-                    Get.back();
-                    WristCheckSnackBars.collectionDeletedSnackbar();
+                  },
+                ),
+                const Divider(thickness: 2,),
+                 ListTile(
+                  title: const Text("Show Archived Watches"),
+                    leading: const Icon(Icons.archive_outlined),
+                  onTap: (){
+                    Get.to(()=> Archived());
                   }
+                ),
+                const Divider(thickness: 2,),
+                ListTile(
+                  title:const Text("Delete collection"),
+                  leading: const Icon(Icons.warning),
+                  trailing: OutlinedButton(
+                    child: const Icon(Icons.delete, color: Colors.redAccent),
+                    onPressed: (){
+                      Get.defaultDialog(
+                        title: "Warning",
+                        middleText: "Pressing OK will delete all watch data, including your wishlist\n \n THIS CANNOT BE UNDONE",
+                        textConfirm: "OK",
+                        textCancel: "Cancel",
+                        onConfirm: (){
+                          //On pressing ok, call the .clear() method on the watchbox
+                          watchBox.clear();
+                          Get.back();
+                          WristCheckSnackBars.collectionDeletedSnackbar();
+                        }
 
-                );
-              }
-            )
+                      );
+                    }
+                  )
+                ),
+                const Divider(thickness: 2,),
+              ],
+            ),
           ),
-          const Divider(thickness: 2,),
+
+          SizedBox(
+            height: 50,
+            child: Text("App Version: $_buildVersion")
+          )
         ],
       ),
 
 
     );
   }
+
+  Future<String> _getBuildVersion() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    return packageInfo.version;
+
+  }
+
 }
