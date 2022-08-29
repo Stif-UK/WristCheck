138acb60148a77dfda53c8eff95e248afb09be3f
Grant Hutchison <stif@grants-mbp.lan>
2022-08-29 16:45:26 +0100

    updates to acknowledgements

--- lib/copy/aboutapp_copy.dart
+++ lib/copy/aboutapp_copy.dart
@@ -30,7 +30,7 @@ class AboutAppCopy{
       padding: const EdgeInsets.all(12.0),
       child: Column(
         children: const [
-          Text("This application is build with the use of the following libraries:",
+          Text("This application is build with the use of the following libraries and services:",
           style: TextStyle(fontWeight: FontWeight.bold),),
           Divider(thickness: 2),
           Text("GetX has been used for state management and page routing. "
@@ -52,6 +52,13 @@ class AboutAppCopy{
           Text("Image_Cropper library has been used to allow cropping images uploaded to the app"
               "\nAvailable at pub.dev/packages/image_cropper  \n"
               "Distributed under the BSD-3-clause licence."),
+          Divider(thickness: 2),
+          Text("App Icons were generated for all platforms with the help of easyappicon.com"
+              "\nUsing assets created by Umar Irshad available from iconfinder.com"),
+          Divider(thickness: 2),
+          Text("With thanks to the users and contributors of the Christopher Ward Forum."
+              "\nTheir input and support was invaluable to the development of this application.",
+            style: TextStyle(fontWeight: FontWeight.bold),),
           Divider(thickness: 2)
         ],
       ),
