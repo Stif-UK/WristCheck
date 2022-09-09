5c83b50f9ffd914ab7b088b047ee7ecf677c3963
Grant Hutchison <stif@grants-mbp.lan>
2022-09-09 10:04:37 +0100

    implemented selection of initial directory

--- lib/model/backup_restore_methods.dart
+++ lib/model/backup_restore_methods.dart
@@ -2,30 +2,39 @@ import 'dart:io';
 import 'package:hive/hive.dart';
 import 'package:file_picker/file_picker.dart';
 
-Future<void> backupHiveBox<T>(String boxName, String backupPath) async {
-  final box = await Hive.openBox<T>(boxName);
-  final boxPath = box.path;
-  await box.close();
-
-  try {
-    File(boxPath!).copy(backupPath);
+class BackupRestoreMethods {
+  static Future<String?> pickBackupLocation() async {
+    String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+    if (selectedDirectory == null) {
+      return null;
+    } else {
+      return selectedDirectory;
+    }
   }
-  catch(e){
-    print("Caught exception: $e");
-  }
-  finally {
-    await Hive.openBox<T>(boxName);
-  }
-}
 
-Future<void> restoreHiveBox<T>(String boxName, String backupPath) async {
-  final box = await Hive.openBox<T>(boxName);
-  final boxPath = box.path;
-  await box.close();
+  static Future<void> backupHiveBox<T>(String boxName, String backupPath) async {
+    final box = await Hive.openBox<T>(boxName);
+    final boxPath = box.path;
+    await box.close();
 
-  try {
-    File(backupPath).copy(boxPath!);
-  } finally {
-    await Hive.openBox<T>(boxName);
+    try {
+      File(boxPath!).copy(backupPath);
+    } catch (e) {
+      print("Caught exception: $e");
+    } finally {
+      await Hive.openBox<T>(boxName);
+    }
+  }
+
+  static Future<void> restoreHiveBox<T>(String boxName, String backupPath) async {
+    final box = await Hive.openBox<T>(boxName);
+    final boxPath = box.path;
+    await box.close();
+
+    try {
+      File(backupPath).copy(boxPath!);
+    } finally {
+      await Hive.openBox<T>(boxName);
+    }
   }
 }
--- lib/ui/backup_restore.dart
+++ lib/ui/backup_restore.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:wristcheck/copy/dialogs.dart';
+import 'package:wristcheck/model/backup_restore_methods.dart';
 
-class BackupRestore extends StatelessWidget {
+class BackupRestore extends StatefulWidget {
    BackupRestore({Key? key}) : super(key: key);
 
+  @override
+  State<BackupRestore> createState() => _BackupRestoreState();
+}
+
+class _BackupRestoreState extends State<BackupRestore> {
   String lastBackup = "No backup recorded";
+
+  String? backUpLocation = "Not Selected";
 
   @override
   Widget build(BuildContext context) {
@@ -28,12 +36,22 @@ class BackupRestore extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             SizedBox(
-              height: MediaQuery.of(context).size.height*0.25,
+              height: MediaQuery.of(context).size.height*0.10,
                 width: MediaQuery.of(context).size.width,
                 child: Text("Last Backup: $lastBackup", textAlign: TextAlign.center,)),
+            SizedBox(
+                height: MediaQuery.of(context).size.height*0.15,
+                width: MediaQuery.of(context).size.width,
+                child: Text("Backup Location: $backUpLocation", textAlign: TextAlign.center,)),
             ElevatedButton(
                 child: const Text("Backup Database"),
-                onPressed: (){}, ),
+                onPressed: (){
+                  setState(() {
+                    BackupRestoreMethods.pickBackupLocation().then((value) => backUpLocation = value);
+                  });
+
+                }, //onPressed
+            ),
             Padding(
               padding: const EdgeInsets.fromLTRB(0,30, 0, 0),
               child: ElevatedButton(
