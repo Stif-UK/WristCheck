f0f08378191c32a5ac0721c75e09d3fefe0acec5
Grant Hutchison <stif@grants-mbp.lan>
2022-09-24 20:16:54 +0100

    added google_mobile_ads dependency and created config file

---
+++ lib/config.dart
@@ -0,0 +1,8 @@
+class WristCheckConfig{
+  /*
+  prodBuild can be used to determine if the app is in prod or test mode for builds
+  for example, this can be set to false and then checked by the app to show test
+  ads rather than using the prod ad units.
+   */
+  static bool prodBuild = false;
+}
