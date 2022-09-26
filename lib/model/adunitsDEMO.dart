ebcc88d7229f677e0efed017b7da4d4b9cf9d0ba
Grant Hutchison <stif@grants-mbp.lan>
2022-09-26 16:52:08 +0100

    Implemented ads on wishlist page

--- lib/model/adunitsDEMO.dart
+++ lib/model/adunitsDEMO.dart
@@ -25,4 +25,8 @@ class AdUnitsDEMO{
   static String get favouritesPageBannerAdUnitId => Platform.isAndroid
       ? "ca-app-pub-3940256099942544/6300978111"
       : "ca-app-pub-3940256099942544/2934735716";
+
+  static String get wishlistPageBannerAdUnitId => Platform.isAndroid
+      ? "ca-app-pub-3940256099942544/6300978111"
+      : "ca-app-pub-3940256099942544/2934735716";
 }
