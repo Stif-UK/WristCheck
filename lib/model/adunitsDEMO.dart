641e2006ce70963b99205a0956946a1f9438654c
Grant Hutchison <stif@grants-mbp.lan>
2022-09-26 18:59:23 +0100

    Implemented ads on archive screen

--- lib/model/adunitsDEMO.dart
+++ lib/model/adunitsDEMO.dart
@@ -37,4 +37,8 @@ class AdUnitsDEMO{
   static String get notificationsPageBannerAdUnitId => Platform.isAndroid
       ? "ca-app-pub-3940256099942544/6300978111"
       : "ca-app-pub-3940256099942544/2934735716";
+
+  static String get archivePageBannerAdUnitId => Platform.isAndroid
+      ? "ca-app-pub-3940256099942544/6300978111"
+      : "ca-app-pub-3940256099942544/2934735716";
 }
--- lib/ui/archived.dart
+++ lib/ui/archived.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
 import 'package:wristcheck/boxes.dart';
 import 'package:get/get.dart';
+import 'package:wristcheck/config.dart';
+import 'package:wristcheck/model/adunits.dart';
+import 'package:wristcheck/model/wristcheck_preferences.dart';
+import 'package:wristcheck/provider/adstate.dart';
 import 'package:wristcheck/ui/view_watch.dart';
 import 'package:wristcheck/model/watches.dart';
 import 'package:wristcheck/copy/snackbars.dart';
 import 'package:wristcheck/copy/dialogs.dart';
+import 'package:wristcheck/util/ad_widget_helper.dart';
 
 class Archived extends StatefulWidget {
-  Archived({Key? key}) : super(key: key);
+  const Archived({Key? key}) : super(key: key);
 
   @override
   State<Archived> createState() => _ArchivedState();
@@ -17,6 +24,28 @@ class Archived extends StatefulWidget {
 
 class _ArchivedState extends State<Archived> {
   var watchBox = Boxes.getWatches();
+  BannerAd? banner;
+  bool purchaseStatus = WristCheckPreferences.getAppPurchasedStatus() ?? false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if(!purchaseStatus)
+    {
+      final adState = Provider.of<AdState>(context);
+      adState.initialization.then((status) {
+        setState(() {
+          banner = BannerAd(
+              adUnitId: WristCheckConfig.prodBuild == false? adState.getTestAds : AdUnits.archivePageBannerAdUnitId,
+              //If the device screen is large enough display a larger ad on this screen
+              size: AdSize.largeBanner,
+              request: const AdRequest(),
+              listener: adState.adListener)
+            ..load();
+        });
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,63 +61,71 @@ class _ArchivedState extends State<Archived> {
         ],
 
       ),
-        body: ValueListenableBuilder<Box<Watches>>(
-            valueListenable: watchBox.listenable(),
-            builder: (context, box, _){
-              List<Watches> archiveList = Boxes.getArchivedWatches();
+        body: Column(
+          children: [
+            Expanded(
+              child: ValueListenableBuilder<Box<Watches>>(
+                  valueListenable: watchBox.listenable(),
+                  builder: (context, box, _){
+                    List<Watches> archiveList = Boxes.getArchivedWatches();
 
 
 
-              return archiveList.isEmpty?Container(
-                alignment: Alignment.center,
-                child: const Text("Your Archive is currently empty",
-                  textAlign: TextAlign.center,),
-              ):
+                    return archiveList.isEmpty?Container(
+                      alignment: Alignment.center,
+                      child: const Text("Your Archive is currently empty",
+                        textAlign: TextAlign.center,),
+                    ):
 
-              ListView.separated(
-                itemCount: archiveList.length,
-                itemBuilder: (BuildContext context, int index){
-                  final item = archiveList[index].toString();
-                  var watch = archiveList.elementAt(index);
-                  String? _title = "${watch.manufacturer} ${watch.model}";
-                  String? _status = "${watch.status}";
-
-
-                  return Dismissible(
-                    key: Key(item),
-                    direction: DismissDirection.endToStart,
-
-                    onDismissed: (direction) {
-                      setState(() {
-                        archiveList.removeAt(index);
-                        var watchInfo = "${watch.manufacturer} ${watch.model}";
-                        watchBox.delete(watch.key);
-                        //ToDo: Delete any images associated with the watch
-                        // Then show a snackbar.
-                        WristCheckSnackBars.deleteWatch(watchInfo);
-                      });
-
-                    },
-
-                    child: ListTile(
-                      leading: const Icon(Icons.watch),
-                      title: Text(_title),
-                      subtitle: Text(_status),
-                      onTap: () => Get.to(() => ViewWatch(currentWatch: watch,)),
-                    ),
-
-                    background: Container(
-                    alignment: Alignment.center,color: Colors.red,
-                    child: const Text("Deleting"),),
-                  );
-                },
-                separatorBuilder: (context, index){
-                  return const Divider();
-                },
-              );
-            }
+                    ListView.separated(
+                      itemCount: archiveList.length,
+                      itemBuilder: (BuildContext context, int index){
+                        final item = archiveList[index].toString();
+                        var watch = archiveList.elementAt(index);
+                        String? _title = "${watch.manufacturer} ${watch.model}";
+                        String? _status = "${watch.status}";
 
 
+                        return Dismissible(
+                          key: Key(item),
+                          direction: DismissDirection.endToStart,
+
+                          onDismissed: (direction) {
+                            setState(() {
+                              archiveList.removeAt(index);
+                              var watchInfo = "${watch.manufacturer} ${watch.model}";
+                              watchBox.delete(watch.key);
+                              //ToDo: Delete any images associated with the watch
+                              // Then show a snackbar.
+                              WristCheckSnackBars.deleteWatch(watchInfo);
+                            });
+
+                          },
+
+                          child: ListTile(
+                            leading: const Icon(Icons.watch),
+                            title: Text(_title),
+                            subtitle: Text(_status),
+                            onTap: () => Get.to(() => ViewWatch(currentWatch: watch,)),
+                          ),
+
+                          background: Container(
+                          alignment: Alignment.center,color: Colors.red,
+                          child: const Text("Deleting"),),
+                        );
+                      },
+                      separatorBuilder: (context, index){
+                        return const Divider();
+                      },
+                    );
+                  }
+
+
+              ),
+            ),
+            purchaseStatus? const SizedBox(height: 0,) : AdWidgetHelper.buildLargeAdSpace(banner, context),
+            const SizedBox(height: 100,)
+          ],
         )
     );
   }
