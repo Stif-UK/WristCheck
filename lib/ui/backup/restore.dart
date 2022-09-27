3818b5b86187ad26d5fd8df5454b6bbe17d5820b
Grant Hutchison <stif@grants-mbp.lan>
2022-09-27 17:16:15 +0100

    implemented ads on Restore page

--- lib/ui/backup/restore.dart
+++ lib/ui/backup/restore.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:provider/provider.dart';
+import 'package:wristcheck/config.dart';
 import 'dart:io';
 import 'package:wristcheck/copy/dialogs.dart';
+import 'package:wristcheck/model/adunits.dart';
 import 'package:wristcheck/model/backup_restore_methods.dart';
+import 'package:wristcheck/model/wristcheck_preferences.dart';
+import 'package:wristcheck/provider/adstate.dart';
+import 'package:wristcheck/util/ad_widget_helper.dart';
 
 class Restore extends StatefulWidget {
   const Restore({Key? key}) : super(key: key);
@@ -12,6 +19,28 @@ class Restore extends StatefulWidget {
 
 class _RestoreState extends State<Restore> {
   File? _backupFile;
+  BannerAd? banner;
+  bool purchaseStatus = WristCheckPreferences.getAppPurchasedStatus() ?? false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if(!purchaseStatus)
+    {
+      final adState = Provider.of<AdState>(context);
+      adState.initialization.then((status) {
+        setState(() {
+          banner = BannerAd(
+              adUnitId: WristCheckConfig.prodBuild == false? adState.getTestAds : AdUnits.restorePageBannerAdUnitID,
+              //If the device screen is large enough display a larger ad on this screen
+              size: AdSize.banner,
+              request: const AdRequest(),
+              listener: adState.adListener)
+            ..load();
+        });
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,33 +57,41 @@ class _RestoreState extends State<Restore> {
         ],
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          const SizedBox(height: 20,),
-          const Text("Please select backup file"),
-          const SizedBox(height: 20,),
-          ElevatedButton(
-            child: const Text("Select Backup File"),
-          onPressed: () async{
-              await BackupRestoreMethods.pickBackupFile().then((value) {
-                setState(() {
-                  _backupFile = value;
-                });
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20,),
+                const Text("Please select backup file"),
+                const SizedBox(height: 20,),
+                ElevatedButton(
+                  child: const Text("Select Backup File"),
+                onPressed: () async{
+                    await BackupRestoreMethods.pickBackupFile().then((value) {
+                      setState(() {
+                        _backupFile = value;
+                      });
 
-              });
-          },
+                    });
+                },
+                ),
+                const SizedBox(height: 20,),
+                _backupFile != null? Text("File selected: $_backupFile. \n\nReady to load"): const Text(""),
+                const Divider(thickness: 2,),
+                const SizedBox(height: 20,),
+                _backupFile == null? const SizedBox(height: 20,): ElevatedButton(
+                    child: const Text("Restore from Backup"),
+                    onPressed: (){
+                      WristCheckDialogs.getConfirmRestoreDialog(_backupFile!);
+                    }
+                 ),
+              ],
+            ),
           ),
-          const SizedBox(height: 20,),
-          _backupFile != null? Text("File selected: $_backupFile. \n\nReady to load"): const Text(""),
-          const Divider(thickness: 2,),
-          const SizedBox(height: 20,),
-          _backupFile == null? const SizedBox(height: 20,): ElevatedButton(
-              child: const Text("Restore from Backup"),
-              onPressed: (){
-                WristCheckDialogs.getConfirmRestoreDialog(_backupFile!);
-              }
-           ),
+          purchaseStatus? const SizedBox(height: 0,) : AdWidgetHelper.buildSmallAdSpace(banner, context),
+          const SizedBox(height: 50,)
         ],
       ),
     );
