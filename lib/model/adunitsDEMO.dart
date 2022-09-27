cfff69f8f61a26095f600872518c7556c9e614a4
Grant Hutchison <stif@grants-mbp.lan>
2022-09-27 16:37:35 +0100

    Implemented ads on backup page

--- lib/model/adunitsDEMO.dart
+++ lib/model/adunitsDEMO.dart
@@ -45,4 +45,8 @@ class AdUnitsDEMO{
   static String get backupLandingBannerAdUnitId => Platform.isAndroid
       ? "ca-app-pub-3940256099942544/6300978111"
       : "ca-app-pub-3940256099942544/2934735716";
+
+  static String get backupPageBannerAdUnitID => Platform.isAndroid
+      ? "ca-app-pub-3940256099942544/6300978111"
+      : "ca-app-pub-3940256099942544/2934735716";
 }
--- lib/ui/backup/backup.dart
+++ lib/ui/backup/backup.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:provider/provider.dart';
+import 'package:wristcheck/config.dart';
 import 'dart:io';
 import 'package:wristcheck/copy/dialogs.dart';
+import 'package:wristcheck/model/adunits.dart';
 import 'package:wristcheck/model/backup_restore_methods.dart';
+import 'package:wristcheck/model/wristcheck_preferences.dart';
+import 'package:wristcheck/provider/adstate.dart';
+import 'package:wristcheck/util/ad_widget_helper.dart';
 
 class Backup extends StatefulWidget {
   const Backup({Key? key}) : super(key: key);
@@ -15,6 +22,29 @@ class _BackupState extends State<Backup> {
   bool _backupComplete = false;
   bool _loadedLocation = false;
   bool _loadedBackup = false;
+  BannerAd? banner;
+  bool purchaseStatus = WristCheckPreferences.getAppPurchasedStatus() ?? false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if(!purchaseStatus)
+    {
+      final adState = Provider.of<AdState>(context);
+      adState.initialization.then((status) {
+        setState(() {
+          banner = BannerAd(
+              adUnitId: WristCheckConfig.prodBuild == false? adState.getTestAds : AdUnits.backupPageBannerAdUnitID,
+              //If the device screen is large enough display a larger ad on this screen
+              size: AdSize.banner,
+              request: const AdRequest(),
+              listener: adState.adListener)
+            ..load();
+        });
+      });
+    }
+  }
+
 
 
   @override
@@ -32,89 +62,97 @@ class _BackupState extends State<Backup> {
         ],
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Expanded(
-            flex: 4,
             child: Column(
-
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const SizedBox(
-                  height: 20,
-                ),
+                Expanded(
+                  flex: 4,
+                  child: Column(
 
-                //Section 1: Select Backup Location - no other sections are displayed until a backup location is selected.
-                const Text("Please Select Backup Location"),
-                const SizedBox(height: 20,),
+                    children: [
+                      const SizedBox(
+                        height: 20,
+                      ),
 
-                //If Platform is Android give some guidance on backup location to minimise errors
-                Platform.isAndroid? const Text("Backup location must be a sub-folder of the Android OS 'Documents' folder",
-                    textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold),) : const Text("We advise creating a new folder for backups, separate to the WristCheck app folder",
-                  textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold),),
-                const SizedBox(height: 20,),
-                ElevatedButton(
-                    child: const Text("Select Backup Location"),
-                    onPressed: () async {
-                      await BackupRestoreMethods.pickBackupLocation().then((val) {
-                        setState(() {
-                          _backupLocation = val;
-                        });
-                      });
-                    }, ),
+                      //Section 1: Select Backup Location - no other sections are displayed until a backup location is selected.
+                      const Text("Please Select Backup Location"),
+                      const SizedBox(height: 20,),
 
-                 //Only show icon if backup location is set, getSuccessIcon returns a loading spinner for a short time, then a tick
-                _backupLocation == null? const SizedBox(height: 40,) : getSuccessIconLocation(_loadedLocation),
-
-                const Divider(height: 15,
-                  thickness: 2,),
-
-                //Section 2: Backup. Only displays if the backup location is selected and the 'tick' icon has had time to load
-                _backupLocation == null || _loadedLocation == false? const SizedBox(height: 20,): Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(
-                        child: const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text("Backup Database",style: TextStyle(fontSize: 30,),),
-                        ),
-                        onPressed: () async {
-                          setState(() {
-                            _backupComplete = true;
-                          });
-
-                          Future.delayed(const Duration(seconds: 2),() async {
-                            await BackupRestoreMethods.backupWatchBox(_backupLocation!).then((_){
-                              setState((
-                                  ) {
-                                _loadedBackup = true;
+                      //If Platform is Android give some guidance on backup location to minimise errors
+                      Platform.isAndroid? const Text("Backup location must be a sub-folder of the Android OS 'Documents' folder",
+                          textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold),) : const Text("We advise creating a new folder for backups, separate to the WristCheck app folder",
+                        textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold),),
+                      const SizedBox(height: 20,),
+                      ElevatedButton(
+                          child: const Text("Select Backup Location"),
+                          onPressed: () async {
+                            await BackupRestoreMethods.pickBackupLocation().then((val) {
+                              setState(() {
+                                _backupLocation = val;
                               });
-                            }
+                            });
+                          }, ),
 
-                            ).onError((error, stackTrace) => WristCheckDialogs.getBackupFailedDialog(error.toString()));
-                          } );
+                       //Only show icon if backup location is set, getSuccessIcon returns a loading spinner for a short time, then a tick
+                      _backupLocation == null? const SizedBox(height: 40,) : getSuccessIconLocation(_loadedLocation),
+
+                      const Divider(height: 15,
+                        thickness: 2,),
+
+                      //Section 2: Backup. Only displays if the backup location is selected and the 'tick' icon has had time to load
+                      _backupLocation == null || _loadedLocation == false? const SizedBox(height: 20,): Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              child: const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text("Backup Database",style: TextStyle(fontSize: 30,),),
+                              ),
+                              onPressed: () async {
+                                setState(() {
+                                  _backupComplete = true;
+                                });
+
+                                Future.delayed(const Duration(seconds: 2),() async {
+                                  await BackupRestoreMethods.backupWatchBox(_backupLocation!).then((_){
+                                    setState((
+                                        ) {
+                                      _loadedBackup = true;
+                                    });
+                                  }
+
+                                  ).onError((error, stackTrace) => WristCheckDialogs.getBackupFailedDialog(error.toString()));
+                                } );
 
 
-                          
-                          
-                        }, ),
-                    ),
-                    _backupComplete? getSuccessIconBackup(_loadedBackup): const SizedBox(height: 40,),
-                    const Divider(height: 15,
-                      thickness: 2,),
-                  ],
+
+
+                              }, ),
+                          ),
+                          _backupComplete? getSuccessIconBackup(_loadedBackup): const SizedBox(height: 40,),
+                          const Divider(height: 15,
+                            thickness: 2,),
+                        ],
+                      ),
+
+
+
+
+                    ],
+                  ),
                 ),
-
-
-
-
+                const Expanded(
+                    flex:2,
+                    child: Icon(Icons.file_download, size: 70,)),
               ],
             ),
           ),
-          const Expanded(
-              flex:4,
-              child: Icon(Icons.file_download, size: 70,)),
+          purchaseStatus? const SizedBox(height: 0,) : AdWidgetHelper.buildSmallAdSpace(banner, context),
+          const SizedBox(height: 50,)
         ],
       ),
     );
