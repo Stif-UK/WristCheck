bdc79cc7a36b1c7bc4f4e0f79ae1eaea67d8f12f
Grant Hutchison <stif@grants-mbp.lan>
2023-09-19 09:49:25 +0100

    expanding the list of movements available

--- lib/model/enums/movement_enum.dart
+++ lib/model/enums/movement_enum.dart
@@ -1,1 +1,1 @@
-enum MovementEnum {blank, mechanical, automatic, quartz}
+enum MovementEnum {blank, mechanical, automatic, analogue_quartz, digital_quartz, ana_digi_quartz, kinetic, mechaquartz, smartwatch, tourbillon, solar, tuning_fork, other}
--- lib/util/wristcheck_formatter.dart
+++ lib/util/wristcheck_formatter.dart
@@ -50,13 +50,40 @@ class WristCheckFormatter{
         returnText = "Not Entered";
         break;
       case MovementEnum.mechanical:
-        returnText = "Mechanical";
+        returnText = "Mechanical - Manual";
         break;
       case MovementEnum.automatic:
-        returnText = "Automatic";
+        returnText = "Mechanical - Automatic";
         break;
-      case MovementEnum.quartz:
-        returnText = "Quartz";
+      case MovementEnum.analogue_quartz:
+        returnText = "Analogue Quartz";
+        break;
+      case MovementEnum.digital_quartz:
+        returnText = "Digital Quartz";
+        break;
+      case MovementEnum.ana_digi_quartz:
+        returnText = "Ana-Digi Quartz";
+        break;
+      case MovementEnum.kinetic:
+        returnText = "Kinetic";
+        break;
+      case MovementEnum.mechaquartz:
+        returnText = "Mecha-Quartz";
+        break;
+      case MovementEnum.smartwatch:
+        returnText = "Smartwatch";
+        break;
+      case MovementEnum.tourbillon:
+        returnText = "Tourbillon";
+        break;
+      case MovementEnum.solar:
+        returnText = "Solar Quartz";
+        break;
+      case MovementEnum.tuning_fork:
+        returnText = "Tuning Fork";
+        break;
+      case MovementEnum.other:
+        returnText = "Other";
         break;
     }
     return returnText;
@@ -69,14 +96,41 @@ class WristCheckFormatter{
       case "Not Entered":
         returnValue = MovementEnum.blank;
         break;
-      case "Mechanical":
+      case "Mechanical - Manual":
         returnValue = MovementEnum.mechanical;
         break;
-      case "Automatic":
+      case "Mechanical - Automatic":
         returnValue = MovementEnum.automatic;
         break;
-      case "Quartz":
-        returnValue = MovementEnum.quartz;
+      case "Analogue Quartz":
+        returnValue = MovementEnum.analogue_quartz;
+        break;
+      case "Digital Quartz":
+        returnValue = MovementEnum.digital_quartz;
+        break;
+      case "Ana-Digi Quartz":
+        returnValue = MovementEnum.ana_digi_quartz;
+        break;
+      case "Kinetic":
+        returnValue = MovementEnum.kinetic;
+        break;
+      case "Mecha-Quartz":
+        returnValue = MovementEnum.mechaquartz;
+        break;
+      case "Smartwatch":
+        returnValue = MovementEnum.smartwatch;
+        break;
+      case "Tourbillon":
+        returnValue = MovementEnum.tourbillon;
+        break;
+      case "Solar Quartz":
+        returnValue = MovementEnum.solar;
+        break;
+      case "Tuning Fork":
+        returnValue = MovementEnum.tuning_fork;
+        break;
+      case "Other":
+        returnValue = MovementEnum.other;
         break;
       default:
         returnValue = MovementEnum.blank;
