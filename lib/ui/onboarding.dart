2788d719f6d24a08bc5729700671a72f2eb96c82
Grant Hutchison <stif@grants-mbp.lan>
2023-09-26 16:13:44 +0100

    setting up page template for demo slides

--- lib/ui/onboarding.dart
+++ lib/ui/onboarding.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:get/get.dart';
 
 class WristCheckOnboarding extends StatefulWidget {
   const WristCheckOnboarding({Key? key}) : super(key: key);
@@ -8,25 +10,110 @@ class WristCheckOnboarding extends StatefulWidget {
 }
 
 class _WristCheckOnboardingState extends State<WristCheckOnboarding> {
+  final pageViewController = PageController();
+
+  @override
+  void dispose() {
+    pageViewController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        children: [
-          Container(
-            color: Colors.red,
-            child: const Center(child: Text("Page 1")),
-          ),
-          Container(
-            color: Colors.blue,
-            child: const Center(child: Text("Page 2")),
-          ),
-          Container(
-            color: Colors.green,
-            child: const Center(child: Text("Page 3")),
-          ),
-        ],
+      body: Container(
+        padding: const EdgeInsets.only(bottom: 80),
+        child: PageView(
+          controller: pageViewController,
+          children: [
+            buildPage(
+                colour: Theme.of(context).canvasColor,
+                urlImage: 'assets/demo/page1_logo.png',
+                title: "Welcome to WristCheck!",
+                subtitle: "lorem ipsum dolar sit amet, consectetur adipiscing elit, sed do eiusmod, tempopr, incididunt, ut labore et dolore manda aliqua."),
+            Container(
+              color: Colors.blue,
+              child: const Center(child: Text("Page 2")),
+            ),
+            Container(
+              color: Colors.green,
+              child: const Center(child: Text("Page 3")),
+            ),
+          ],
+        ),
+      ),
+      bottomSheet:  Container(
+        padding: const EdgeInsets.all(10.0),
+        height: 80,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextButton(
+                child: const Text("SKIP"),
+            onPressed: (){
+                  pageViewController.jumpToPage(2);
+            },),
+            Center(
+              child: SmoothPageIndicator(
+                  controller: pageViewController,
+                  effect: SlideEffect(
+                    type: SlideType.slideUnder,
+                    dotColor: Get.isDarkMode? Colors.white24: Colors.black26,
+                    activeDotColor: Color(0xff39a5c0),
+                  ),
+                  onDotClicked: (index) => pageViewController.animateToPage(
+                      index,
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.easeIn),
+                  count: 3),
+            ),
+            TextButton(
+              child: const Text("NEXT"),
+              onPressed: (){
+                pageViewController.nextPage(
+                    duration: const Duration(microseconds: 500),
+                    curve: Curves.easeInOut);
+              },),
+          ],
+        ),
       ),
     );
   }
+  Widget buildPage({
+    required Color colour,
+    required String urlImage,
+    required String title,
+    required String subtitle
+  }) => Container(
+    color: colour,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          urlImage,
+          fit: BoxFit.cover,
+          width: double.infinity,
+        ),
+        const SizedBox(height: 40,),
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            color: Color(0xff39a5c0),
+            fontSize: 32,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        const SizedBox(height: 24,),
+        Container(
+          padding: const EdgeInsets.symmetric(),
+          child: Text(
+            subtitle,
+              textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.bodyLarge
+          ),
+        )
+      ],
+    ),
+  );
 }
