3f91e39e33e522b02827c016ecd5f8397ff5f825
Grant Hutchison <stif@grants-mbp.lan>
2023-09-29 16:54:16 +0100

    splitting collection stats into pages

--- lib/ui/collection_stats.dart
+++ lib/ui/collection_stats.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:wristcheck/ui/collection_stats/collection_charts.dart';
 import 'package:wristcheck/ui/collection_stats/collection_info.dart';
 import 'package:wristcheck/copy/dialogs.dart';
 
 
-class CollectionStats extends StatelessWidget {
+class CollectionStats extends StatefulWidget {
   const CollectionStats({Key? key}) : super(key: key);
 
+  @override
+  State<CollectionStats> createState() => _CollectionStatsState();
+}
+
+class _CollectionStatsState extends State<CollectionStats> {
+
+  int _currentIndex = 0;
+  final List<Widget> _children =[
+    const CollectionInfo(),
+    const CollectionCharts()
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +32,27 @@ class CollectionStats extends StatelessWidget {
           IconButton(onPressed: (){WristCheckDialogs.getCollectionStatsDialog();}, icon: const Icon(Icons.help_outline))
         ],
       ),
-      body: const CollectionInfo()
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon:  Icon(FontAwesomeIcons.clipboardList),
+            label: "Info",
+          ),
+          BottomNavigationBarItem(
+            icon:  Icon(FontAwesomeIcons.chartBar),
+            label: "Charts",
+          ),
+        ],
+      ),
     );
   }
 
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 }
---
+++ lib/ui/collection_stats/collection_charts.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CollectionCharts extends StatefulWidget {
+  const CollectionCharts({Key? key}) : super(key: key);
+
+  @override
+  State<CollectionCharts> createState() => _CollectionChartsState();
+}
+
+class _CollectionChartsState extends State<CollectionCharts> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text("Stuff here"),
+    );
+  }
+}
