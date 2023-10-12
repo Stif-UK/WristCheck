a730193309ffe9234898eb4eecd55aae762c952f
Grant Hutchison <stif@grants-mbp.lan>
2023-10-12 18:26:14 +0100

    implementing firebase and crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
@@ -15,9 +17,26 @@ import 'package:wristcheck/provider/db_provider.dart';
 import 'package:wristcheck/api/purchase_api.dart';
 import 'package:json_theme/json_theme.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
+  //Initialise Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
 
   //Initialise Ads
   final initFuture = MobileAds.instance.initialize();
