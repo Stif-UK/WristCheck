810befec09d75fbb94d9b89e90e2f30780ec9cce
Grant Hutchison <stif@grants-mbp.lan>
2024-01-13 22:28:08 +0000

    implemented ability to amend privacy consent

--- lib/ui/watch_home_drawer.dart
+++ lib/ui/watch_home_drawer.dart
@@ -7,6 +7,7 @@ import 'package:wristcheck/controllers/wristcheck_controller.dart';
 import 'package:wristcheck/ui/SettingsPage.dart';
 import 'package:wristcheck/ui/PrivacyPolicy.dart';
 import 'package:wristcheck/ui/AboutApp.dart';
+import 'package:wristcheck/ui/privacy_landing.dart';
 import 'package:wristcheck/ui/remove_ads.dart';
 import 'package:wristcheck/util/general_helper.dart';
 
@@ -45,9 +46,9 @@ class WatchHomeDrawer extends StatelessWidget {
           const Divider(thickness: 2,),
           ListTile(
             trailing: const Icon(Icons.warning_amber_rounded),
-            title: const Text("Privacy Policy"),
+            title: const Text("Privacy"),
             onTap: (){
-              Get.to(() => PrivacyPolicy());
+              Get.to(() => PrivacyLanding());
             },
           ),
           const Divider(thickness: 2,),
