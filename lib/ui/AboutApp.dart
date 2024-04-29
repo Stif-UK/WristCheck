8cca035cc9acd07634f68b7345b0f27832cb9af9
Grant Hutchison <stif@grants-mbp.lan>
2024-04-29 16:27:55 +0100

    updating about pages with icons and external link to website

--- lib/ui/AboutApp.dart
+++ lib/ui/AboutApp.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:wristcheck/copy/aboutapp_copy.dart';
 import 'package:wristcheck/copy/whats_new_copy.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutApp extends StatelessWidget{
 
@@ -24,7 +26,17 @@ class AboutApp extends StatelessWidget{
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            ListTile(
+              leading: Icon(FontAwesomeIcons.earthAmericas),
+              title: Text("Visit www.wristcheck.app"),
+              trailing: Icon(FontAwesomeIcons.arrowUpRightFromSquare),
+              onTap: () async {
+                _launchURL();
+              },
+            ),
+            const Divider(thickness: 2,),
             ExpansionTile(title: const Text("About WristCheck"),
+            leading: Icon(FontAwesomeIcons.clock),
             onExpansionChanged: (bool) async {
               await analytics.logEvent(name: "about_wc_expanded",
               parameters: {
@@ -34,7 +46,9 @@ class AboutApp extends StatelessWidget{
             children: [
               AboutAppCopy.getAboutWristCheckCopy(),
             ],),
+            const Divider(thickness: 2,),
             ExpansionTile(title: const Text("About the Developer"),
+              leading: Icon(FontAwesomeIcons.code),
               onExpansionChanged: (bool) async {
                 await analytics.logEvent(name: "about_stifdev_expanded",
                     parameters: {
@@ -44,7 +58,9 @@ class AboutApp extends StatelessWidget{
             children: [
               AboutAppCopy.getAboutDeveloperCopy(),
             ],),
+            const Divider(thickness: 2,),
             ExpansionTile(title: const Text("Acknowledgements"),
+              leading: Icon(FontAwesomeIcons.award),
               onExpansionChanged: (bool) async {
                 await analytics.logEvent(name: "ack_expanded",
                     parameters: {
@@ -55,7 +71,9 @@ class AboutApp extends StatelessWidget{
               AboutAppCopy.getAcknowledgementCopy(),
 
             ],),
+            const Divider(thickness: 2,),
             ExpansionTile(title: const Text("Version History"),
+              leading: Icon(FontAwesomeIcons.clockRotateLeft),
               onExpansionChanged: (bool) async {
                 await analytics.logEvent(name: "version_history_expanded",
                     parameters: {
@@ -65,12 +83,19 @@ class AboutApp extends StatelessWidget{
               children: [
                 WhatsNewCopy.getVersionHistory(context),
 
-              ],)
+              ],),
+            const Divider(thickness: 2,),
           ],
         ),
       ),
 
     );
   }
-
+  void _launchURL() async {
+    final Uri url = Uri.parse('https://www.wristcheck.app');
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
+  }
 }
+
