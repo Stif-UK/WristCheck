ce4bdea83820679cc4357efcde478e070fbaa0ae
Grant Hutchison <stif@grants-mbp.lan>
2024-05-02 08:42:40 +0100

    updating simple extract code and adding comment documentation to the class methods

--- lib/model/extract_methods.dart
+++ lib/model/extract_methods.dart
@@ -8,16 +8,21 @@ import 'package:path_provider/path_provider.dart';
 
 class ExtractMethods{
 
+  /**
+   * The generateSimpleExtract() method iterates over the users collection and generates a CSV String containing all key watch details
+   * with a count of total times worn. It then internally calls the shareExtract() method to trigger the generation and sharing of a CSV file
+   * which the user can then access as required.
+   */
   static generateSimpleExtract() async {
     List<Watches> watchbox = Boxes.getAllWatches();
 
     //Produce a list of Rows to convert
     List<List<String?>> csvList = [];
     //Create Header Row
-    csvList.add(["Status", "Manufacturer", "Model", "Category", "Serial Number", "Reference Number", "Movement", "Purchase Date", "Warranty Expiry Date", "Last Serviced Date", "Purchase Price", "Purchased From", "Sold Price", "Sold To", "Notes"]);
+    csvList.add(["Status", "Manufacturer", "Model", "Category", "Serial Number", "Reference Number", "Movement", "Purchase Date", "Warranty Expiry Date", "Last Serviced Date", "Purchase Price", "Purchased From", "Sold Price", "Sold To", "Notes", "Tracked Wear Count"]);
     //Add additional rows per watch
     for(Watches watch in watchbox){
-      csvList.add([watch.status!, watch.manufacturer, watch.model, watch.category, watch.serialNumber, watch.referenceNumber, watch.movement, watch.purchaseDate.toString(), watch.warrantyEndDate.toString(), watch.lastServicedDate.toString(), watch.purchasePrice.toString(), watch.purchasedFrom, watch.soldPrice.toString(), watch.soldTo, watch.notes]);
+      csvList.add([watch.status!, watch.manufacturer, watch.model, watch.category, watch.serialNumber, watch.referenceNumber, watch.movement, watch.purchaseDate.toString(), watch.warrantyEndDate.toString(), watch.lastServicedDate.toString(), watch.purchasePrice.toString(), watch.purchasedFrom, watch.soldPrice.toString(), watch.soldTo, watch.notes, watch.wearList.length.toString()]);
     }
     String csv = const ListToCsvConverter().convert(csvList);
     print(csv);
@@ -25,6 +30,10 @@ class ExtractMethods{
 
   }
 
+  /**
+   * The shareExtract() method takes a csv in the form of a String, generates a file called 'wristcheck_extract.csv'
+   * and passes this to the OS share dialog, allowing the user to then save or transfer the file as required.
+   */
   static Future<ShareResult> shareExtract(String csv) async {
     final directory = await getApplicationDocumentsDirectory();
     final File file = File('${directory.path}/wristcheck_extract.csv');
