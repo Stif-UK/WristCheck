c5a32f39fa73493b93ceb18fbca3f7d4775165b4
Grant Hutchison <stif@grants-mbp.lan>
2024-05-08 11:45:28 +0100

    prototyping month chart

--- lib/ui/charts/watch_months.dart
+++ lib/ui/charts/watch_months.dart
@@ -23,54 +23,39 @@ class _WatchMonthChartState extends State<WatchMonthChart> {
   Widget build(BuildContext context) {
 
     //Calculate the chart data - generate a map of months and counts
-    Map<String,int> chartData = <String,int>{};
-    //Add all months
+    Map<int,int> chartData = <int,int>{};
+    //Populate Months
+    for(int i = 1 ; i <= 12; i++ ){
+      chartData[i] = 0;
+    }
+    //Populate Counts
+    for(DateTime wearDate in widget.currentWatch.wearList){
+      int month = wearDate.month;
+      chartData.update(month, (value) => ++value);
+    }
+    print(chartData);
 
 
 
-    // for(var watch in data){
-    //   if(watch.category != null){
-    //     chartData.update(
-    //       watch.category!,
-    //           (value) => ++value,
-    //       ifAbsent: () => 1,
-    //     );
-    //   }
-    // }
-    //remove 'not selected' and empty string if it exists
-    // if (chartData.containsKey("Not Selected")) {
-    //   chartData.remove("Not Selected");
-    // }
-    // if (chartData.containsKey("")) {
-    //   chartData.remove("");
-    // }
+    List<CategoryData> getChartData = [];
 
-    // //sort map
-    // var sortedChartData = Map.fromEntries(
-    //     chartData.entries.toList()..sort((e1, e2) => e1.value.compareTo(e2.value)));
-    //
-    //
-    // List<CategoryData> getChartData = [];
-    //
-    // for(var item in sortedChartData.entries){
-    //   getChartData.add(CategoryData(item.key, item.value));
-    // }
-    return Container(
-      child: Text("Chart goes here"),
+    for(var item in chartData.entries){
+      getChartData.add(CategoryData(item.key.toString(), item.value));
+    }
+
+
+    return SfCartesianChart(
+      series: <ChartSeries>[
+        BarSeries<CategoryData, String>(
+          dataSource: getChartData,
+          xValueMapper: (CategoryData mvmt, _) => mvmt.category,
+          yValueMapper: (CategoryData mvmt, _) => mvmt.count,
+          dataLabelMapper: (moov, _)=> "${moov.category}: ${moov.count}",
+          dataLabelSettings: const DataLabelSettings(isVisible: true),
+        )
+      ],
+      primaryXAxis: CategoryAxis(isVisible: false),
     );
-
-    // return SfCartesianChart(
-    //   series: <ChartSeries>[
-    //     BarSeries<CategoryData, String>(
-    //       dataSource: getChartData,
-    //       xValueMapper: (CategoryData mvmt, _) => mvmt.category,
-    //       yValueMapper: (CategoryData mvmt, _) => mvmt.count,
-    //       dataLabelMapper: (moov, _)=> "${moov.category}: ${moov.count}",
-    //       dataLabelSettings: const DataLabelSettings(isVisible: true),
-    //     )
-    //   ],
-    //   primaryXAxis: CategoryAxis(isVisible: false),
-    // );
   }
 }
 
