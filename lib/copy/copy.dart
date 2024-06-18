c72c1d9d42cf783583897e1737cf01f536ca3684
Grant Hutchison <stif@grants-mbp.lan>
2024-06-18 11:33:09 +0100

    updating references to app name

--- lib/copy/copy.dart
+++ lib/copy/copy.dart
@@ -11,7 +11,7 @@ class WristCheckCopy {
         child: Markdown(
             shrinkWrap: true,
             data: "**Alternative Data Extracts**\n\n"
-                "These options allow your watch and wear data to be extracted from **WristCheck**\n\n"
+                "These options allow your watch and wear data to be extracted from **WristTrack**\n\n"
                 "They are meant to free your data, rather than as a backup _(see the Backup/Restore options if you are simply looking to move data from one device to another)._\n\n"
                 "The simple extract provides a list of all watch data, including wear count and notes **(one row per watch)**\n\n"
                 "The detailed extract provides a line of data for each **tracked wear date** and only includes watches which have been tracked as worn.**(multiple rows per watch)**\n\n"
@@ -28,9 +28,9 @@ class WristCheckCopy {
         child: Markdown(
             shrinkWrap: true,
             data: "**Watch Wear Charts**\n\n"
-                "Watch charts are a **WristCheck Pro** feature.\n\n"
+                "Watch charts are a **WristTrack Pro** feature.\n\n"
                 "They allow you to view charts breaking down which months and days this watch has been worn.\n\n"
-                "Want to know more abou **WristCheck Pro**? Click the button below..."
+                "Want to know more about **WristTrack Pro**? Click the button below..."
         ),
       ),
     );
