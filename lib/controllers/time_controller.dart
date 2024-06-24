fa8abe8b2ea321e84be03f3611e978d088de80fb
Grant Hutchison <stif@grants-mbp.lan>
2024-06-24 22:06:34 +0100

    setting up trigger for audible countdown

--- lib/controllers/time_controller.dart
+++ lib/controllers/time_controller.dart
@@ -3,6 +3,7 @@ import 'package:wristcheck/util/wristcheck_formatter.dart';
 import 'dart:async';
 
 class TimeController extends GetxController{
+  final lastBeep = 56.obs;
   final currentDate = "".obs;
   final currentTime = "".obs;
   final currentDateTime = DateTime.now().obs;
@@ -17,17 +18,41 @@ class TimeController extends GetxController{
     enableBeep(beep);
   }
 
+  updateLastBeep(sec){
+    lastBeep(sec);
+  }
+
   updateMilitaryTime(mt){
     militaryTime(mt);
   }
 
   updateTime() {
+    int lastbeep;
     Timer.periodic(Duration(milliseconds: 50), (Timer t) {
       var date = DateTime.now();
+      switch(date.second){
+        case 57:
+          triggerBeep(57);
+          break;
+        case 58:
+          triggerBeep(58);
+          break;
+        case 59:
+          triggerBeep(59);
+          break;
+      }
       currentDateTime(date);
       currentTime(WristCheckFormatter.getTime(date, militaryTime.value));
       currentDate(WristCheckFormatter.getFormattedDateWithDay(date));
     });
 
+  }
+
+  triggerBeep(int current){
+    if(current != lastBeep.value)
+      {
+        print("beep!");
+      };
+    updateLastBeep(current);
   }
 }
