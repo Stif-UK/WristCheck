8922eaf5055dd623ef2b4bcc753997876a90418c
Grant Hutchison <stifdev.uk@gmail.com>
2024-07-24 09:05:03 +0100

    fix FAB not hiding issue

--- lib/ui/wristcheck_home.dart
+++ lib/ui/wristcheck_home.dart
@@ -109,14 +109,17 @@ class _WristCheckHomeState extends State<WristCheckHome> {
       },
 
       //hide FAB except on collection screen
-      floatingActionButton: widget.wristCheckController.homePageIndex.value == 0 ? FloatingActionButton(
-        child: const Icon(Icons.add_rounded),
-        backgroundColor: Colors.red,
-        //onPressed: (){Get.to(() => const AddWatch());},
-        onPressed: () async {
-          await analytics.logEvent(name: "add_watch_fab_pressed");
-          Get.to(() => WatchView());},
-      ): null,
+      floatingActionButton: Obx(() => Container(
+          child: widget.wristCheckController.homePageIndex.value == 0 ? FloatingActionButton(
+            child: const Icon(Icons.add_rounded),
+            backgroundColor: Colors.red,
+            //onPressed: (){Get.to(() => const AddWatch());},
+            onPressed: () async {
+              await analytics.logEvent(name: "add_watch_fab_pressed");
+              Get.to(() => WatchView());},
+          ): null,
+        ),
+      ),
 
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
 
