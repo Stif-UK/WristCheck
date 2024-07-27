89d0ef7280ca672f17e248c3598b98fdbb9eb89a
Grant Hutchison <stifdev.uk@gmail.com>
2024-07-27 22:34:32 +0100

    removing unused imports

--- lib/util/wristcheck_formatter.dart
+++ lib/util/wristcheck_formatter.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:wristcheck/model/enums/category.dart';
 import 'package:wristcheck/model/enums/collection_view.dart';
