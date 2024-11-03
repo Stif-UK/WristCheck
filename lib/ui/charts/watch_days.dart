faf656d10432205fafb49c2c427d8f23495d1589
Grant Hutchison <stifdev.uk@gmail.com>
2024-11-03 20:41:48 +0000

    implement line chart

--- lib/ui/charts/watch_days.dart
+++ lib/ui/charts/watch_days.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 import 'package:wristcheck/controllers/wristcheck_controller.dart';
 import 'package:wristcheck/model/enums/watch_day_chart_enum.dart';
 import 'package:wristcheck/model/watches.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:wristcheck/util/wristcheck_formatter.dart';
 
 class WatchDayChart extends StatefulWidget {
   WatchDayChart({
@@ -22,6 +24,16 @@ class WatchDayChart extends StatefulWidget {
     5 : "Friday",
     6 : "Saturday",
     7 : "Sunday"
+  };
+
+  Map<int, String> shortDayMap = <int, String>{
+    1 : "Mon",
+    2 : "Tue",
+    3 : "Wed",
+    4 : "Thu",
+    5 : "Fri",
+    6 : "Sat",
+    7 : "Sun"
   };
 
 
@@ -61,7 +73,7 @@ class _WatchDayChartState extends State<WatchDayChart> {
         returnChart =_buildGroupedChart();
         break;
       case WatchDayChartEnum.line:
-        returnChart = _buildLineChart();
+        returnChart = _buildLineChart(_getSplitChartData(widget.currentWatch), widget.shortDayMap);
         break;
       default:
         returnChart = _buildBarChart(_getBasicChartData(widget.currentWatch), widget.dayMap);
@@ -113,10 +125,28 @@ Widget _buildGroupedChart(){
   );
 }
 
-Widget _buildLineChart(){
-  return Container(
-    alignment: Alignment.center,
-    child: Text("Line Chart Here"),
+Widget _buildLineChart(Map<int, List<DayWearData>> data, Map<int, String> dayMap){
+  var chartSeries = <StackedLineSeries>[];
+  for(int month in data.keys){
+    StackedLineSeries yearSeries = StackedLineSeries<DayWearData, String>(
+      groupName: month.toString(),
+      name: WristCheckFormatter.getMonthName(month),
+      dataSource: data[month]!,
+      xValueMapper: (DayWearData data, _) => dayMap[data.day],
+      yValueMapper: (DayWearData data, _) => data.count,
+    );
+    chartSeries.add(yearSeries);
+  }
+
+  return SfCartesianChart(
+    primaryXAxis: CategoryAxis(),
+    series: chartSeries,
+    legend: Legend(
+        isVisible: true,
+        position: LegendPosition.top,
+        title: LegendTitle(text: "Month")
+    ),
+
   );
 }
 
@@ -142,6 +172,48 @@ List<DayWearData> _getBasicChartData(Watches currentWatch){
     returnList.add(DayWearData(item.key, item.value));
     }
     return returnList;
+}
+
+Map<int, List<DayWearData>> _getSplitChartData(Watches watch){
+  //First create a list of months
+  List<int> months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+
+  //Map<MONTH, MAP<DAY, COUNT>>
+
+  //Create a map of months with sub-map for days to capture counts
+  Map<int, Map<int, int>> dataMap = {};
+  for(int selectedMonth in months){
+    Map<int,int> chartData = <int,int>{};
+    //Populate Days
+    for(int i = 1 ; i <= 7; i++){
+      chartData[i] = 0;
+    }
+    //Populate Counts
+    for(DateTime wearDate in watch.wearList){
+      if(wearDate.month == selectedMonth) {
+        int day = wearDate.weekday;
+        chartData.update(day, (value) => ++value);
+      }
+    }
+    //Update Map
+    dataMap[selectedMonth] = chartData;
+  }
+
+  //Create return list of data objects
+  Map<int, List<DayWearData>> returnMap = {};
+
+  //Convert to objects and populate list
+  for(var currentMonth in dataMap.entries){
+    List<DayWearData> monthList = [];
+    //we're in the first map, iterate over the second
+    for(var currentDay in dataMap[currentMonth.key]!.entries){
+      monthList.add(DayWearData(currentDay.key, currentDay.value));
+    }
+    returnMap[currentMonth.key]= monthList;
+  }
+  //return the data
+  return returnMap;
 }
 
 class DayWearData{
