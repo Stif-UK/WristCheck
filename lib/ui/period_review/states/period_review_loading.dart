cadfdcf9a37d2d60fbd21b229bd0416649fd948e
Grant Hutchison <stifdev.uk@gmail.com>
2024-12-20 14:17:41 +0000

    importing loading animation

--- lib/ui/period_review/states/period_review_loading.dart
+++ lib/ui/period_review/states/period_review_loading.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class PeriodReviewLoading extends StatelessWidget {
   const PeriodReviewLoading({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Loading");
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text("Hang tight while we crunch some numbers..."),
+          const SizedBox(height: 40,),
+          LoadingAnimationWidget.staggeredDotsWave(color: Theme.of(context).focusColor, size: 50)
+        ],
+      ),
+    );
   }
 }
