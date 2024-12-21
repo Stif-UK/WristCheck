d09e04c7aadad934b2160b8a4a8f39d96d232442
Grant Hutchison <stifdev.uk@gmail.com>
2024-12-21 10:31:53 +0000

    adding wristcheck teal colour as a static variable

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class WristCheckConfig{
   /*
   prodBuild can be used to determine if the app is in prod or test mode for builds
@@ -6,4 +8,10 @@ class WristCheckConfig{
    */
   static bool prodBuild = true;
 
+  /*
+  getWCColour returns the shade of teal used in the WristTrack logo, to make it easily available throughout the app.
+   */
+  static Color getWCColour(){
+    return const Color(0xff39a5c0);
+  }
 }
