fba11b48a4daa87f3ef5aed697fe6a206519aca8
Grant Hutchison <stifdev.uk@gmail.com>
2025-01-16 11:14:27 +0000

    updating text on landing page

--- lib/ui/period_review/states/period_review_landing.dart
+++ lib/ui/period_review/states/period_review_landing.dart
@@ -6,6 +6,7 @@ import 'package:wristcheck/model/enums/review_state.dart';
 class PeriodReviewLanding extends StatelessWidget {
   PeriodReviewLanding({super.key});
   final reviewController = Get.put(ReviewController());
+  final double padInsets = 20.0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +15,20 @@ class PeriodReviewLanding extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(padInsets),
             child: Text("Welcome to the WristTrack Wrist Recap!\n\n"
                 "Here you can generate a summary of your wrist habits over a selected period.\n\n",
             style: Theme.of(context).textTheme.bodyLarge,),
           ),
-          ElevatedButton(child: Text("Generate 2024 Wrist Recap"),
-          onPressed: () =>reviewController.updateReviewState(ReviewState.loading),)
+          ElevatedButton(child: Text("Generate Wrist Recap"),
+          onPressed: () =>reviewController.updateReviewState(ReviewState.loading),),
+          Padding(
+            padding: EdgeInsets.all(padInsets),
+            child: Text("Note: This feature is currently in BETA and will be enhanced over time.\n\n"
+                "Results can currently only be generated for years with at least 30 days of tracked wear.\n\n"
+                "Please feel free to reach out with feedback!",
+              style: Theme.of(context).textTheme.bodyMedium,),
+          )
         ],
       ),
     );
