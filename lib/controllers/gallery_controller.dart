a734d9da8d112e7f1701f71e272f8daa641315a2
Grant Hutchison <stifdev.uk@gmail.com>
2025-03-08 12:57:17 +0000

    Handling empty image lists

--- lib/controllers/gallery_controller.dart
+++ lib/controllers/gallery_controller.dart
@@ -4,9 +4,16 @@ import 'package:wristcheck/model/enums/gallery_selection_enum.dart';
 class GalleryController extends GetxController{
 
   final gallerySelection = GallerySelectionEnum.watchbox.obs;
+  final dataAvailable = true.obs;
 
   updateGallerySelection(GallerySelectionEnum selection){
     gallerySelection(selection);
+    dataAvailable(true);
   }
+
+  updateDataAvailable(bool available){
+    dataAvailable(available);
+  }
+
 
 }
--- lib/ui/more_menu/gallery/gallery.dart
+++ lib/ui/more_menu/gallery/gallery.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:wristcheck/controllers/gallery_controller.dart';
 import 'package:wristcheck/model/enums/gallery_selection_enum.dart';
 import 'package:wristcheck/model/watches.dart';
@@ -48,69 +49,72 @@ class _GalleryState extends State<Gallery> {
                   );
                 } else if (snapshot.hasData){
                   final data = snapshot.data as List<Watches>;
+                  widget.galleryController.updateDataAvailable(data.isNotEmpty);
                   return SingleChildScrollView(
                     child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Text("Show off your collection!",
-                              textAlign: TextAlign.center,
-                              ),
-                          ),
-                          _getCollectionPickerRow(),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ElevatedButton(
-                              child: Text("Go",
-                              style: TextStyle(fontSize: 20.0),),
-                              //TODO: Handle empty list
-                              onPressed: ()=> SwipeImageGallery(
-                                context: context,
-                                itemBuilder: (context, index){
-                                  return FutureBuilder(
-                                      builder: (ctx, snapshot) {
-                                        if (snapshot.connectionState ==
-                                            ConnectionState.done) {
-                                          if (snapshot.hasError) {
-                                            return Center(
-                                              child: Text(
-                                                "${snapshot.error} occurred",
-                                                style: Theme
-                                                    .of(context)
-                                                    .textTheme
-                                                    .headlineSmall,),
-                                            );
-                                          } else if (snapshot.hasData) {
-                                            final imageData = snapshot.data as File;
-                                            return Image.file(imageData);
-                                          }
-                                        }
-                                        return Center(
-                                          child: CircularProgressIndicator(),
-                                        );
-
-                                      },
-                                        future: ImagesUtil.getImage(data[index], true));
-                                  },
-                                  itemCount: data.length,
-                                // children: data,
-                                onSwipe: (index) {
-                                  overlayController.add(ImageOverlay(
-                                    title: '${index + 1}/${data.length}',
-                                    subtitle: "${data[index].manufacturer} ${data[index].model}",
-                                  ));
-                                },
-                                overlayController: overlayController,
-                                initialOverlay: ImageOverlay(
-                                  title: '1/${data.length}',
-                                  subtitle: "${data[0].manufacturer} ${data[0].model}",
-                              )).show(),
+                      child: Obx(()=> Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Text("Show off your collection!",
+                                textAlign: TextAlign.center,
+                                ),
                             ),
-                          )
-                        ],
+                            _getCollectionPickerRow(),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ElevatedButton(
+                                child: Text("Go",
+                                style: TextStyle(fontSize: 20.0),),
+                                //TODO: Handle empty list
+                                onPressed: ()=> data.isNotEmpty? SwipeImageGallery(
+                                  context: context,
+                                  itemBuilder: (context, index){
+                                    return FutureBuilder(
+                                        builder: (ctx, snapshot) {
+                                          if (snapshot.connectionState ==
+                                              ConnectionState.done) {
+                                            if (snapshot.hasError) {
+                                              return Center(
+                                                child: Text(
+                                                  "${snapshot.error} occurred",
+                                                  style: Theme
+                                                      .of(context)
+                                                      .textTheme
+                                                      .headlineSmall,),
+                                              );
+                                            } else if (snapshot.hasData) {
+                                              final imageData = snapshot.data as File;
+                                              return Image.file(imageData);
+                                            }
+                                          }
+                                          return Center(
+                                            child: CircularProgressIndicator(),
+                                          );
+
+                                        },
+                                          future: ImagesUtil.getImage(data[index], true));
+                                    },
+                                    itemCount: data.length,
+                                  // children: data,
+                                  onSwipe: (index) {
+                                    overlayController.add(ImageOverlay(
+                                      title: '${index + 1}/${data.length}',
+                                      subtitle: "${data[index].manufacturer} ${data[index].model}",
+                                    ));
+                                  },
+                                  overlayController: overlayController,
+                                  initialOverlay: ImageOverlay(
+                                    title: '1/${data.length}',
+                                    subtitle: "${data[0].manufacturer} ${data[0].model}",
+                                )).show() : null ,
+                              ),
+                            ),
+                            widget.galleryController.dataAvailable.value? SizedBox(height: 0,): _getDataNotAvailableRow()
+                          ],
+                        ),
                       ),
                     ),
                   );
@@ -145,6 +149,24 @@ Widget _getCollectionPickerRow(){
       ),
       ),
     ],
+  );
+}
+
+Widget _getDataNotAvailableRow(){
+  return Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Icon(FontAwesomeIcons.eyeSlash),
+        ),
+        Text("No Images were found for this filter",
+          style: Theme.of(Get.context!).textTheme.headlineSmall ,
+        textAlign: TextAlign.center,)
+      ],
+    ),
   );
 }
 
