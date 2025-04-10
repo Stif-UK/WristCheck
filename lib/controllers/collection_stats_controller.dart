ef5ee93e160154a3388dbe4a874403ae4df7205b
Grant Hutchison <stifdev.uk@gmail.com>
2025-04-10 11:00:09 +0100

    implementing saved preference for cost per wear value chart

--- lib/controllers/collection_stats_controller.dart
+++ lib/controllers/collection_stats_controller.dart
@@ -1,11 +1,12 @@
 import 'package:get/get.dart';
 import 'package:wristcheck/model/enums/collection_chart_enums/case_thickness_chart_enum.dart';
 import 'package:wristcheck/model/enums/collection_chart_enums/lug2lug_chart_enum.dart';
+import 'package:wristcheck/model/wristcheck_preferences.dart';
 
 class CollectionStatsController extends GetxController{
   final caseThicknessChartType = CaseThicknessChartEnum.line.obs;
   final lug2lugChartType = Lug2lugChartEnum.line.obs;
-  final showPrice = true.obs;//
+  final showPrice = WristCheckPreferences.getCostPerWearValuePref().obs;//
 
   updateCaseThicknessChartType(type){
     caseThicknessChartType(type);
@@ -17,5 +18,6 @@ class CollectionStatsController extends GetxController{
 
   updateShowPrice(price){
     showPrice(price);
+    WristCheckPreferences.setCostPerWearValuePref(price);
   }
 }
