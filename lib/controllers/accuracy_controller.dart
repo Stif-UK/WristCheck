2972e110d48e2d4e547270bd3f1fd7d1e6c862ed
Grant Hutchison <stifdev.uk@gmail.com>
2025-10-30 17:30:27 +0000

    adding 24hr toggle and creating buttons to capture records

--- lib/controllers/accuracy_controller.dart
+++ lib/controllers/accuracy_controller.dart
@@ -1,8 +1,11 @@
 import 'package:get/get.dart';
+import 'package:wristcheck/model/wristcheck_preferences.dart';
 
 class AccuracyController extends GetxController{
   final watchDateTime = DateTime.now().obs;
   final baseLine = true.obs;
+  final militaryTime = WristCheckPreferences.getMilitaryTime().obs;
+
 
   updateWatchDateTime(DateTime time){
     watchDateTime(time);
@@ -24,5 +27,10 @@ class AccuracyController extends GetxController{
   subtractAMinute(){
     DateTime newTime = watchDateTime.value.subtract(Duration(minutes: 1));
     watchDateTime(newTime);
+  }
+
+  updateMilitaryTime(bool mt) async {
+    await WristCheckPreferences.setMilitaryTime(mt);
+    militaryTime(mt);
   }
 }
--- lib/ui/Accuracy.dart
+++ lib/ui/Accuracy.dart
@@ -28,19 +28,61 @@ class _AccuracyState extends State<Accuracy> {
         children: [
           Obx(()=> Text("Time now: $now, \nPlus one: $nowPlus, \nController: ${widget.accuracyController.watchDateTime}")),
           Obx(()=> SwitchListTile(
-            title: const Text("Baseline measurement:"),
+            title: const Text("24 Hour Display:"),
+              value: widget.accuracyController.militaryTime.value,
+              onChanged:(value) =>widget.accuracyController.updateMilitaryTime(value) )),
+          Obx(()=> SwitchListTile(
+              title: const Text("Baseline measurement:"),
               value: widget.accuracyController.baseLine.value,
               onChanged:(value) =>widget.accuracyController.updateBaseline(value) )),
           IconButton(
             icon: Icon(FontAwesomeIcons.caretUp),
             onPressed: ()=> widget.accuracyController.addAMinute(),
           ),
-          Obx(()=> Text(WristCheckFormatter.getShortTime(widget.accuracyController.watchDateTime.value, false),
+          Obx(()=> Text(WristCheckFormatter.getShortTime(widget.accuracyController.watchDateTime.value, widget.accuracyController.militaryTime.value),
             style: Theme.of(context).textTheme.headlineLarge ,)),
           IconButton(
             icon: Icon(FontAwesomeIcons.caretDown),
             onPressed: ()=> widget.accuracyController.subtractAMinute(),
           ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("00 seconds", style: Theme.of(context).textTheme.headlineSmall,),
+              ),
+              onPressed: (){},
+            ),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8.0, 15.0, 25.0, 15.0),
+                child: ElevatedButton(
+                  child: Text("45 seconds", style: Theme.of(context).textTheme.bodyLarge,),
+                  onPressed: (){},
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25.0, 15.0, 8.0, 15.0),
+                child: ElevatedButton(
+                  child: Text("15 seconds",style: Theme.of(context).textTheme.bodyLarge,),
+                  onPressed: (){},
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              child: Text("30 seconds", style: Theme.of(context).textTheme.bodyLarge,),
+              onPressed: (){},
+            ),
+          ),
+          const Divider(thickness: 2,)
         ],
       ),
     );
