1b5daec1f3a4bd16559b05b13d4535cbf998b61c
Grant Hutchison <stifdev.uk@gmail.com>
2025-11-07 22:02:59 +0000

    adding ability to mark an image as primary

--- lib/ui/widgets/bottomsheets/image_update_bottomsheet.dart
+++ lib/ui/widgets/bottomsheets/image_update_bottomsheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:wristcheck/copy/dialogs.dart';
+import 'package:wristcheck/model/watch_methods.dart';
 import 'package:wristcheck/model/watches.dart';
 import 'package:wristcheck/util/images_util.dart';
 
@@ -11,55 +12,67 @@ class ImageUpdateBottomsheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white38,
-        shape: BoxShape.rectangle,
-        borderRadius: BorderRadius.circular(15),
+    int primaryIndex = watch.primaryImageIndex ?? 0;
+    return SingleChildScrollView(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white38,
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        height: MediaQuery.of(context).size.height*0.6,
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            //Header#
+      
+      
+            Row(
+              children: [
+                Expanded(
+                  child: Text("${watch.toString()}\nImage ${index + 1}",
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  textAlign: TextAlign.center,),
+                ),
+      
+              ],
+            ),
+            const SizedBox(height: 20,),
+            ListTile(
+                title: Text("Primary Image"),
+                trailing: primaryIndex == index? Icon(FontAwesomeIcons.solidStar, color: Colors.blue,) : Icon(FontAwesomeIcons.star, color: Colors.blue,),
+                onTap: () async {
+                  Navigator.pop(context);
+                  WatchMethods.setPrimaryImage(watch, index);
+                }
+            ),
+            const Divider(thickness: 2,),
+            ListTile(
+              title: Text("Update Image"),
+              trailing: Icon(FontAwesomeIcons.repeat, color: Colors.green,),
+              onTap: () async {
+                Navigator.pop(context);
+                await ImagesUtil.addImageViaController(index, context, watch);
+              }
+            ),
+            const Divider(thickness: 2,),
+            ListTile(
+              title: Text("Delete Image"),
+              trailing: Icon(FontAwesomeIcons.trash, color: Colors.red,),
+              onTap: ()=> WristCheckDialogs.showImageDeleteDialog(context, watch, index),
+            ),
+            const Divider(thickness: 2,),
+            ListTile(
+              title: Text("Cancel"),
+              onTap: ()=> Navigator.pop(context),
+              trailing: Icon(FontAwesomeIcons.ban),
+            )
+      
+          ],
+        ),
+      
       ),
-      height: MediaQuery.of(context).size.height*0.4,
-      padding: const EdgeInsets.all(15),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          //Header#
-
-
-          Row(
-            children: [
-              Expanded(
-                child: Text("${watch.toString()}\nImage ${index + 1}",
-                  style: Theme.of(context).textTheme.headlineSmall,
-                textAlign: TextAlign.center,),
-              ),
-
-            ],
-          ),
-          const SizedBox(height: 20,),
-          ListTile(
-            title: Text("Update Image"),
-            trailing: Icon(FontAwesomeIcons.repeat, color: Colors.green,),
-            onTap: () async {
-              Navigator.pop(context);
-              await ImagesUtil.addImageViaController(index, context, watch);
-            }
-          ),
-          const Divider(thickness: 2,),
-          ListTile(
-            title: Text("Delete Image"),
-            trailing: Icon(FontAwesomeIcons.trash, color: Colors.red,),
-            onTap: ()=> WristCheckDialogs.showImageDeleteDialog(context, watch, index),
-          ),
-          const Divider(thickness: 2,),
-          ListTile(
-            title: Text("Cancel"),
-            onTap: ()=> Navigator.pop(context),
-            trailing: Icon(FontAwesomeIcons.ban),
-          )
-
-        ],
-      ),
-
     );
   }
 }
