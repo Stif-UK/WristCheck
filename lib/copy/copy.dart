bff7130ebd203ac70b025d986d88b862aa889427
Grant Hutchison <stifdev.uk@gmail.com>
2025-11-07 12:40:52 +0000

    implemented help information for accuracy tracking

--- lib/copy/copy.dart
+++ lib/copy/copy.dart
@@ -56,17 +56,28 @@ class WristCheckCopy {
       child: const Padding(
         padding: EdgeInsets.all(12.0),
         child: Markdown(
+          physics: ClampingScrollPhysics(),
             shrinkWrap: true,
             data:
             "Track your watches accuracy by creating checkpoints - "
-                "WristTrack can then compare the change in the time on your watch,"
+                "WristTrack can then compare the change in the time on your watch"
                 " to the change in time from the atomic clock and calculate if it is gaining or losing time\n\n"
                 "**Baselines**\n\n"
                 "When you set a checkpoint as a baseline, all following measurements"
-                " will be compared to it. You should set a new baseline any time you have manually adjusted your watch since the last baseline."
+                " will be compared to it. You should set a new baseline any time you have manually adjusted your watch since the last baseline.\n\n"
                 "If you have no records saved, the first result is always tagged as a baseline record.\n\n"
-                "**Capturing a measurement**\n\n"
-                "lorem ipsum"
+                "**Capturing a Measurement**\n\n"
+                "To capture a data point, set the time value under 'add checkpoint' "
+                "to match your watch is going to be (it defaults to a minute ahead) and then press the '00 seconds' button when the seconds hand reaches twelve o'clock. "
+                "Alternatively, set the time to match your watch and use the '15/30/45 seconds' buttons when the seconds hand passes those to capture the timestamp.\n\n "
+                "The times captured will then appear in the 'Records' section below, along with an accuracy calculated since the last baseline record (no accuracy value shows for baselines).\n\n"
+                "**Deleting a Record**\n\n"
+                "If you capture a record in error, it can be deleted by swiping it away from right to left in the 'Records' list.\n\n"
+                "**When to capture records**\n\n"
+                "The longer you track the watch from the baseline measurement, the more accurate the results are likely to be "
+                "(as the small delays when pressing buttons become less prominent). As a guide, it's useful to leave 12-24 hours between measurements.\n\n"
+                "_*You can re-open this information box at any time by pressing the question mark in the top right of the page*_\n\n "
+
         ),
       ),
     );
--- lib/ui/widgets/bottomsheets/accuracy_help_bottomsheet.dart
+++ lib/ui/widgets/bottomsheets/accuracy_help_bottomsheet.dart
@@ -8,29 +8,31 @@ class AccuracyHelpBottomsheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-          color: Colors.white38,
-          shape: BoxShape.rectangle,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        padding: const EdgeInsets.all(15),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-            //Header#
-              Row(
-                children: [
-                  Expanded(child: Text("Watch Accuracy", style: Theme.of(context).textTheme.headlineSmall,
-                  textAlign: TextAlign.center,)),
-                  IconButton(icon: Icon(FontAwesomeIcons.x),
-                  onPressed: Get.back,)
-                ],
-              ),
-              const Divider(thickness: 2,),
-              SingleChildScrollView(child: WristCheckCopy.getAccuracyHelpCopy()),
-
-            ]),);
+    return SingleChildScrollView(
+      child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white38,
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          padding: const EdgeInsets.all(15),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+              //Header#
+                Row(
+                  children: [
+                    Expanded(child: Text("Watch Accuracy", style: Theme.of(context).textTheme.headlineSmall,
+                    textAlign: TextAlign.center,)),
+                    IconButton(icon: Icon(FontAwesomeIcons.x),
+                    onPressed: Get.back,)
+                  ],
+                ),
+                const Divider(thickness: 2,),
+                WristCheckCopy.getAccuracyHelpCopy(),
+      
+              ]),),
+    );
 
   }
 }
