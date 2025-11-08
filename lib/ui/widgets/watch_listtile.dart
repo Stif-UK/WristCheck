fcadfdf4f19a77d736c9e95650f47fd3384a8ede
Grant Hutchison <stifdev.uk@gmail.com>
2025-11-08 20:51:01 +0000

    setting to show image set as primary

--- lib/ui/widgets/watch_listtile.dart
+++ lib/ui/widgets/watch_listtile.dart
@@ -32,7 +32,7 @@ class WatchListTile extends ListTile {
     return ListTile(
       leading: showImage ?
       FutureBuilder(
-          future: ImagesUtil.getImage(currentWatch, 0),
+          future: ImagesUtil.getImage(currentWatch, currentWatch.primaryImageIndex ?? 0),
           builder: (context, snapshot) {
             //start
             if (snapshot.connectionState == ConnectionState.done) {
