923042ba8d1db39954f4493f4582742295710f17
Grant Hutchison <stifdev.uk@gmail.com>
2025-12-10 17:00:23 +0000

    updating radio buttons in line with latest flutter sdk, removing deprecated values

--- lib/ui/widgets/wear_filters/basic_filters_widget.dart
+++ lib/ui/widgets/wear_filters/basic_filters_widget.dart
@@ -20,165 +20,131 @@ class _BasicFiltersWidgetState extends State<BasicFiltersWidget> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Obx(()=>
-          Column(
-            children: [
-              RadioListTile(
-                  title: const Text("Show all"),
-                  value: WearChartOptions.all,
-                  groupValue: widget.filterController.basicWearFilter.value,
-                  onChanged: (value) async {
-                    await widget.filterController.updateFilterName(value as WearChartOptions);
-                  }
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: RadioListTile(
-                        title: const Text("This Month"),
-                        value: WearChartOptions.thisMonth,
-                        groupValue: widget.filterController.basicWearFilter.value,
-                        onChanged: (value) async {
-                          await widget.filterController.updateFilterName(value as WearChartOptions);
-                        }
+          RadioGroup(
+            groupValue: widget.filterController.basicWearFilter.value,
+            onChanged: (value) async {
+              await widget.filterController.updateFilterName(value as WearChartOptions);
+            },
+            child: Column(
+              children: [
+                RadioListTile(
+                    title: const Text("Show all"),
+                    value: WearChartOptions.all,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: RadioListTile(
+                          title: const Text("This Month"),
+                          value: WearChartOptions.thisMonth,
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: RadioListTile(
-                        title: const Text("Last Month"),
-                        value: WearChartOptions.lastMonth,
-                        groupValue: widget.filterController.basicWearFilter.value,
-                        onChanged: (value) async {
-                          await widget.filterController.updateFilterName(value as WearChartOptions);
-                        }
+                    Expanded(
+                      child: RadioListTile(
+                          title: const Text("Last Month"),
+                          value: WearChartOptions.lastMonth,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: RadioListTile(
-                        title: const Text("This Year"),
-                        value: WearChartOptions.thisYear,
-                        groupValue: widget.filterController.basicWearFilter.value,
-                        onChanged: (value) async {
-                          await widget.filterController.updateFilterName(value as WearChartOptions);
-                        }
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: RadioListTile(
+                          title: const Text("This Year"),
+                          value: WearChartOptions.thisYear,
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: RadioListTile(
-                        title: const Text("Last Year"),
-                        value: WearChartOptions.lastYear,
-                        groupValue: widget.filterController.basicWearFilter.value,
-                        onChanged: (value) async {
-                          await widget.filterController.updateFilterName(value as WearChartOptions);
-                        }
+                    Expanded(
+                      child: RadioListTile(
+                          title: const Text("Last Year"),
+                          value: WearChartOptions.lastYear,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: RadioListTile(
-                        title: const Text("Last 30 days"),
-                        value: WearChartOptions.last30days,
-                        groupValue: widget.filterController.basicWearFilter.value,
-                        onChanged: (value) async {
-                          await widget.filterController.updateFilterName(value as WearChartOptions);
-                        }
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: RadioListTile(
+                          title: const Text("Last 30 days"),
+                          value: WearChartOptions.last30days,
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: RadioListTile(
-                        title: const Text("Last 90 days"),
-                        value: WearChartOptions.last90days,
-                        groupValue: widget.filterController.basicWearFilter.value,
-                        onChanged: (value) async {
-                          await widget.filterController.updateFilterName(value as WearChartOptions);
-                        }
+                    Expanded(
+                      child: RadioListTile(
+                          title: const Text("Last 90 days"),
+                          value: WearChartOptions.last90days,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: RadioListTile(
-                        title: const Text("Last 365 days"),
-                        value: WearChartOptions.last365days,
-                        groupValue: widget.filterController.basicWearFilter.value,
-                        onChanged: (value) async {
-                          await widget.filterController.updateFilterName(value as WearChartOptions);
-                        }
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: RadioListTile(
+                          title: const Text("Last 365 days"),
+                          value: WearChartOptions.last365days,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: RadioListTile(
-                        title: const Text("Since Last Purchase"),
-                        value: WearChartOptions.lastPurchase,
-                        groupValue: widget.filterController.basicWearFilter.value,
-                        onChanged: (value) async {
-                          await widget.filterController.updateFilterName(value as WearChartOptions);
-                        }
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: RadioListTile(
+                          title: const Text("Since Last Purchase"),
+                          value: WearChartOptions.lastPurchase,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              RadioListTile(
-                  title: const Text("Select Month/Year"),
-                  value: WearChartOptions.manual,
-                  groupValue: widget.filterController.basicWearFilter.value,
-                  onChanged: (value) async {
-                    await widget.filterController.updateFilterName(value as WearChartOptions);
-                  }
-              ),
-              widget.filterController.basicWearFilter.value == WearChartOptions.manual? Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 25),
-                    child: Text("Month:", style: Theme.of(context).textTheme.bodyLarge,),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15),
-                    child: DropdownButton(
-                      value: widget.filterController.selectedMonth.value,
-                      items: MonthList.values.map((month) => DropdownMenuItem(
-                          value: month,
-                          child:Text(WristCheckFormatter.getMonthText(month),) )).toList(),
-                      onChanged: (month){
-                        widget.filterController.updateSelectedMonth(month as MonthList);
-                      },
+                  ],
+                ),
+                RadioListTile(
+                    title: const Text("Select Month/Year"),
+                    value: WearChartOptions.manual,
+                ),
+                widget.filterController.basicWearFilter.value == WearChartOptions.manual? Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25),
+                      child: Text("Month:", style: Theme.of(context).textTheme.bodyLarge,),
                     ),
-                  )
-                ],
-              ): const SizedBox(height: 0,),
-              widget.filterController.basicWearFilter.value == WearChartOptions.manual? Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 25),
-                    child: Text("Year:", style: Theme.of(context).textTheme.bodyLarge,),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15),
-                    child: DropdownButton(
-                      value: widget.filterController.selectedYear.value,
-                      items: widget.filterController.yearList.map((year) => DropdownMenuItem(
-                          value: year,
-                          child:Text(year) )).toList(),
-                      onChanged: (year){
-                        widget.filterController.updateSelectedYear(year as String);
-                      },
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: DropdownButton(
+                        value: widget.filterController.selectedMonth.value,
+                        items: MonthList.values.map((month) => DropdownMenuItem(
+                            value: month,
+                            child:Text(WristCheckFormatter.getMonthText(month),) )).toList(),
+                        onChanged: (month){
+                          widget.filterController.updateSelectedMonth(month as MonthList);
+                        },
+                      ),
+                    )
+                  ],
+                ): const SizedBox(height: 0,),
+                widget.filterController.basicWearFilter.value == WearChartOptions.manual? Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25),
+                      child: Text("Year:", style: Theme.of(context).textTheme.bodyLarge,),
                     ),
-                  )
-                ],
-              ): const SizedBox(height: 0,)
-            ],
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: DropdownButton(
+                        value: widget.filterController.selectedYear.value,
+                        items: widget.filterController.yearList.map((year) => DropdownMenuItem(
+                            value: year,
+                            child:Text(year) )).toList(),
+                        onChanged: (year){
+                          widget.filterController.updateSelectedYear(year as String);
+                        },
+                      ),
+                    )
+                  ],
+                ): const SizedBox(height: 0,)
+              ],
+            ),
           ),
       ),
     );
