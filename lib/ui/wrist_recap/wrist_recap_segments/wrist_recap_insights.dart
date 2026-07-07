3a344cc742d79e4265ee3de60e0d23494b3bd934
Grant Hutchison <stifdev.uk@gmail.com>
2026-07-07 14:14:05 +0100

    adding an add at the bottom of the insights section

--- lib/ui/wrist_recap/wrist_recap_segments/wrist_recap_insights.dart
+++ lib/ui/wrist_recap/wrist_recap_segments/wrist_recap_insights.dart
@@ -1,54 +1,110 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:provider/provider.dart';
+import 'package:wristcheck/config.dart';
 import 'package:wristcheck/controllers/wrist_recap_controllers/wrist_recap_monthly_controller.dart';
 import 'package:wristcheck/l10n/app_localizations.dart';
+import 'package:wristcheck/model/adunits.dart';
+import 'package:wristcheck/model/wristcheck_preferences.dart';
+import 'package:wristcheck/provider/adstate.dart';
 import 'package:wristcheck/ui/wrist_recap/wrist_recap_widgets/insight_card.dart';
+import 'package:wristcheck/util/ad_widget_helper.dart';
 
-class WristRecapInsights extends StatelessWidget {
+class WristRecapInsights extends StatefulWidget {
   WristRecapInsights({super.key});
 
+  @override
+  State<WristRecapInsights> createState() => _WristRecapInsightsState();
+}
+
+class _WristRecapInsightsState extends State<WristRecapInsights> {
   final recapController = Get.put(WristRecapMonthlyController());
+  BannerAd? banner;
+  bool purchaseStatus = WristCheckPreferences.getAppPurchasedStatus() ?? false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (!purchaseStatus) {
+      final adState = Provider.of<AdState>(context);
+      adState.initialization.then((status) {
+        if (!mounted) return;
+        setState(() {
+          banner = BannerAd(
+              adUnitId: WristCheckConfig.prodBuild == false
+                  ? adState.getTestAds
+                  : AdUnits.recapInsightsAd,
+              size: AdSize.banner,
+              request: const AdRequest(),
+              listener: adState.adListener)
+            ..load();
+        });
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        elevation: 4,
-        margin: const EdgeInsets.all(8.0),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(AppLocalizations.of(context)!.insightsTitle,style: Theme.of(context).textTheme.bodyLarge),
-              Obx(() => GridView.count(
-                crossAxisCount: 2,
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                children: _getInsights(context),
-              )),
-            ],
-          ),
-        ));
+    return Column(
+      children: [
+        Card(
+            elevation: 4,
+            margin: const EdgeInsets.all(8.0),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(AppLocalizations.of(context)!.insightsTitle,
+                      style: Theme.of(context).textTheme.bodyLarge),
+                  Obx(() => GridView.count(
+                        crossAxisCount: 2,
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        children: _getInsights(context),
+                      )),
+                ],
+              ),
+            )),
+        if (!purchaseStatus) AdWidgetHelper.buildSmallAdSpace(banner, context),
+      ],
+    );
   }
 
-  List<InsightCard> _getInsights(BuildContext context){
+  List<InsightCard> _getInsights(BuildContext context) {
     List<InsightCard> insights = [];
-    if(recapController.watchesWorn.isNotEmpty) {
+    if (recapController.watchesWorn.isNotEmpty) {
       int wearcount = recapController.watchesWorn.length;
       //Get total watches worn
       insights.add(InsightCard(
-          title: AppLocalizations.of(context)!.watchesWornInsightTitle,
-          value: wearcount.toString(),
-      valueBig: true,));
+        title: AppLocalizations.of(context)!.watchesWornInsightTitle,
+        value: wearcount.toString(),
+        valueBig: true,
+      ));
       //Get wears tracked per day
       //TODO: handle months with less than a full track - get first wear entry in the month and if later than the 1st of the month reduce count
-      int days = DateUtils.getDaysInMonth(recapController.year.value, recapController.month.value);
-      insights.add(InsightCard(title: AppLocalizations.of(context)!.wearsPerDayInsightTitle, value: (wearcount/days).toStringAsFixed(1), valueBig: true,));
+      int days = DateUtils.getDaysInMonth(
+          recapController.year.value, recapController.month.value);
+      insights.add(InsightCard(
+          title: AppLocalizations.of(context)!.wearsPerDayInsightTitle,
+          value: (wearcount / days).toStringAsFixed(1),
+          valueBig: true));
       //Top Brand
-      insights.add(InsightCard(title: AppLocalizations.of(context)!.topBrandInsightTitle, value: recapController.brandsWorn.first.manufacturer, valueBig: false,));
-      if(recapController.categoriesWorn.isNotEmpty) insights.add(InsightCard(title: AppLocalizations.of(context)!.topCategoryInsightTitle, value: recapController.categoriesWorn.first.category,
-          valueBig: false));
+      if (recapController.brandsWorn.isNotEmpty) {
+        insights.add(InsightCard(
+          title: AppLocalizations.of(context)!.topBrandInsightTitle,
+          value: recapController.brandsWorn.first.manufacturer,
+          valueBig: false,
+        ));
+      }
+      if (recapController.categoriesWorn.isNotEmpty) {
+        insights.add(InsightCard(
+            title: AppLocalizations.of(context)!.topCategoryInsightTitle,
+            value: recapController.categoriesWorn.first.category,
+            valueBig: false));
+      }
     }
     return insights;
   }
