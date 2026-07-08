1a0294a3d4b437b29c5ff7f47a71545f4b9189c2
Grant Hutchison <stifdev.uk@gmail.com>
2026-07-08 17:26:09 +0100

    implementing ability to switch between bar and donut charts

--- lib/ui/wrist_recap/wrist_recap_segments/status_wear_chart.dart
+++ lib/ui/wrist_recap/wrist_recap_segments/status_wear_chart.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:get/get.dart';
 import 'package:wristcheck/controllers/wrist_recap_controllers/wrist_recap_controller.dart';
 import 'package:wristcheck/l10n/app_localizations.dart';
 import 'package:wristcheck/util/helper_classes.dart';
 
-class StatusWearChart extends StatelessWidget {
+class StatusWearChart extends StatefulWidget {
   StatusWearChart({super.key});
+
+  @override
+  State<StatusWearChart> createState() => _StatusWearChartState();
+}
+
+class _StatusWearChartState extends State<StatusWearChart> {
+  bool isDonut = false;
 
   @override
   Widget build(BuildContext context) {
@@ -19,30 +27,70 @@ class StatusWearChart extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(AppLocalizations.of(context)!.statusChartTitle, style: Theme.of(context).textTheme.bodyLarge),
-            SfCartesianChart(
-              primaryXAxis: CategoryAxis(
-                isVisible: false,
-              ),
-              primaryYAxis: NumericAxis(),
-              series: _getBarSeries()
-            )
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(AppLocalizations.of(context)!.statusChartTitle,
+                    style: Theme.of(context).textTheme.bodyLarge),
+                IconButton(
+                  icon: Icon(
+                    isDonut ? FontAwesomeIcons.chartSimple : FontAwesomeIcons.chartPie,
+                    size: 16,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      isDonut = !isDonut;
+                    });
+                  },
+                ),
+              ],
+            ),
+            isDonut
+                ? SfCircularChart(
+                    legend: Legend(
+                        isVisible: true,
+                        overflowMode: LegendItemOverflowMode.wrap),
+                    series: _getDonutSeries(),
+                  )
+                : SfCartesianChart(
+                    primaryXAxis: CategoryAxis(
+                      isVisible: false,
+                    ),
+                    primaryYAxis: NumericAxis(),
+                    series: _getBarSeries()),
           ],
         ),
       ),
     );
   }
 
-  List<BarSeries<dynamic, dynamic>> _getBarSeries() {
-    final recapController = Get.put(WristRecapController());
+  List<BarSeries<StatusWornClass, String>> _getBarSeries() {
+    final recapController = Get.find<WristRecapController>();
 
     return <BarSeries<StatusWornClass, String>>[
       BarSeries(
         dataSource: recapController.statusWorn.reversed.toList(),
         xValueMapper: (StatusWornClass series, _) => series.status,
         yValueMapper: (StatusWornClass series, _) => series.count,
-        dataLabelMapper: (series, _) => "${series.status} : ${series.count} (${series.percentage})",
+        dataLabelMapper: (series, _) =>
+            "${series.status} : ${series.count} (${series.percentage})",
         dataLabelSettings: const DataLabelSettings(isVisible: true),
+      )
+    ];
+  }
+
+  List<DoughnutSeries<StatusWornClass, String>> _getDonutSeries() {
+    final recapController = Get.find<WristRecapController>();
+
+    return <DoughnutSeries<StatusWornClass, String>>[
+      DoughnutSeries<StatusWornClass, String>(
+        dataSource: recapController.statusWorn.toList(),
+        xValueMapper: (StatusWornClass series, _) => series.status,
+        yValueMapper: (StatusWornClass series, _) => series.count,
+        dataLabelMapper: (series, _) => "${series.percentage}",
+        dataLabelSettings: const DataLabelSettings(
+            isVisible: true, labelPosition: ChartDataLabelPosition.outside),
+        enableTooltip: true,
       )
     ];
   }
