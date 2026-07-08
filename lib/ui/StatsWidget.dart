21df0ab738279cfd3e470bad27adb4b332e02097
Grant Hutchison <stifdev.uk@gmail.com>
2026-07-08 11:56:10 +0100

    updating links to wrist recap

--- lib/ui/StatsWidget.dart
+++ lib/ui/StatsWidget.dart
@@ -10,6 +10,7 @@ import 'package:wristcheck/model/wristcheck_preferences.dart';
 import 'package:wristcheck/ui/collection_stats.dart';
 import 'package:wristcheck/ui/period_review/period_review_home.dart';
 import 'package:wristcheck/ui/wear_stats_v2.dart';
+import 'package:wristcheck/ui/wrist_recap/wrist_recap_home.dart';
 import 'package:wristcheck/ui/wrist_recap/wrist_recap_landing.dart';
 
 import '../provider/adstate.dart';
@@ -143,7 +144,10 @@ class _StatsWidgetState extends State<StatsWidget> {
                           ),
                         ),
                         onPressed: (){
-                          WristCheckConfig.enableMonthlyRecap? Get.to(()=> const WristRecapLanding()):Get.to(()=>const PeriodReviewHome());
+                          //Default to opening WristRecap at last month
+                          DateTime now = DateTime.now();
+                          DateTime lastMonth = DateTime(now.year, now.month-1);
+                          Get.to(()=> WristRecapHome(month: lastMonth.month, year: lastMonth.year, monthView: true));
                         },
                         style: ButtonStyle(
                             shape: WidgetStateProperty.all<RoundedRectangleBorder>(
