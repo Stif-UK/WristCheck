3d7edaa49f1a8b645513ca71f094309b4b1fb32a
Grant Hutchison <stifdev.uk@gmail.com>
2026-08-07 22:00:48 +0100

    version bump to 1.19.5

--- lib/copy/whats_new_copy.dart
+++ lib/copy/whats_new_copy.dart
@@ -5,7 +5,7 @@ class WhatsNewCopy{
 
   static String getLatestVersionCopy(){
     return
-        "### Version 1.19.4 \n"
+        "### Version 1.19.5 \n"
         "\n"
 
             "* Merch Store added - grab a T-shirt and help support the next phase of WristTrack development!\n"
@@ -31,7 +31,7 @@ class WhatsNewCopy{
             physics: ClampingScrollPhysics(),
               data:
                   "## Latest Version:\n"
-                      "### Version 1.19.4 \n"
+                      "### Version 1.19.5 \n"
                       "\n"
 
                       "* Merch Store added - grab a T-shirt and help support the next phase of WristTrack development!\n"
